0a47752809f34288dd4a3b26269a7c0d6ce96f80
Jhm20P <71630698+Jhm20P@users.noreply.github.com>
2021-01-08 23:19:59 +0100

    Update typografi.php

--- lærings-portfolio/typografi.php
+++ lærings-portfolio/typografi.php
@@ -1,12 +1,13 @@
 <!doctype html>
-<html lang="en">
+<html lang="da">
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
-    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="author" content="Jonas Hawthorn Mikkelsen">
     <meta name="generator" content="Jekyll v4.1.1">
-    <title>Carousel Template · Bootstrap</title>
+    <link rel="icon" href="favicon.ico">
+    <title>Typografi</title>
 
     <link rel="canonical" href="https://getbootstrap.com/docs/4.5/examples/carousel/">
 
@@ -15,9 +16,11 @@
 
     <!-- Bootstrap core CSS -->
 <link href="assets/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- EGEN CSS -->
 <link rel="stylesheet" href="underside.css">
 <link rel="stylesheet" href="bodyfix.css">
-
+<link rel="stylesheet" href="billede.css">
+    <!-- INTERN CSS -->
     <style>
       .bd-placeholder-img {
         font-size: 1.125rem;
@@ -37,10 +40,11 @@
     <!-- Custom styles for this template -->
     <link href="carousel.css" rel="stylesheet">
   </head>
+    <!-- MENU BAR -->
   <?php include("Menu.inc"); ?>
 
 <main role="main">
-
+<!-- SEKTION MED OVERSKRIFT OG UNDEROVERSKRIFT -->
   <section class="jumbotron text-center">
     <div class="container">
       <h1>Typografi</h1>
@@ -58,41 +62,44 @@
     <!-- START THE FEATURETTES -->
 
     <hr class="featurette-divider">
-
+    <!-- TYPOGRAFI -->
     <div class="row featurette">
       <div class="col-md-7">
-        <h2 class="featurette-heading">First featurette heading. <span class="text-muted">It’ll blow your mind.</span></h2>
-        <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
-      </div>
-      <div class="col-md-5">
-        <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 500x500"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"/><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg>
+        <h2 class="featurette-heading">Typografi.</h2>
+        <p class="lead">Typografi er din tekst, og man vil helst gerne bruge to forskellige skrifttyper når man laver plakater eller hjemmesider eller CV osv. Skrifttyper skel dog helst ligne hinanden, dog hvor den ene af dem er grotesk hvor den anden er antikva. Grotesk bliver mest brugt til overskrifter eller korte esætninger, grundet til at den ikke har de sheriffer som antikva har. Det at antikva har sheriffer gør at de er nemmere at læse og holde sig på den rigtige linje, det er også grunden til at antikva bliver mest brugt til længere sætninger eller brød tekster. Ens skrifttype kan også have en form for personlig eller identitet. Så din skrifttype skal helst passe til det emne du skrive om eller det budskab du gerne vil ud med</p>
       </div>
     </div>
-
+    <!-- TYPOGRAFI SLUT -->
     <hr class="featurette-divider">
-
+    <!-- HELVETICA -->
     <div class="row featurette">
       <div class="col-md-7 order-md-2">
-        <h2 class="featurette-heading">Oh yeah, it’s that good. <span class="text-muted">See for yourself.</span></h2>
-        <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
-      </div>
-      <div class="col-md-5 order-md-1">
-        <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 500x500"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"/><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg>
+        <h2 class="featurette-heading">Helvetica.</h2>
+        <p class="lead">Monotone grotesk blev udgivet i 1926 af Monotype corporation.Helvetica var den mest dominerende fra tidspoerioden 1950s. Denne skrifttype blev brugt på logoer, skilte, posters og tøj. Helvetica er en grotesk skrifttype. Den klarede de forventinger som folk havde i forhold til traditionelt internationalt grafisk design, hvor den ligesom mødte op til de krav. Helvetica er baseret på en typeface der var der i 1896, der har navnet akzidenz-grotesk(brugt som standard i US). Helvetica fik dens navn i 1960, i forsøg på at få den til at sælge mere verden over. Helvetica havde oprindeligt navnet “Neu haas Grotesk”.</p>
+        <p class="lead"><cite>- Nicolaj, Niklas og Jonas, <a href="https://docs.google.com/document/d/1P3lrHxaTCNIvVE2-MEoYMP3ykz-p0jBsJk38YIdXo2U/edit?usp=sharing" target="_blank">typografi præsentation</a>, 25-9-2020</cite></p>
       </div>
     </div>
-
+    <!-- HELVETICA SLUT -->
     <hr class="featurette-divider">
-
+    <!-- HELVETICA -->
     <div class="row featurette">
       <div class="col-md-7">
-        <h2 class="featurette-heading">And lastly, this one. <span class="text-muted">Checkmate.</span></h2>
-        <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
-      </div>
-      <div class="col-md-5">
-        <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 500x500"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"/><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg>
+        <h2 class="featurette-heading">Helvetica.</h2>
+        <p class="lead">Helvetica betyder switzerland på latin(hvilket er der hvor helvetica kommer fra). Helvetica blev skabt af Eduard Hoffmann, som var præsident for Haas type foundry, der befandt sig Switzerland. Grunden til at Eduard Hoffmann skabte Helvetica, var at han var bekymret for at salg af “sans-serif” ville falde, samt det at det var tydeligt for ham at de klassiske designs som han virksomhed lavede, ikke kunne klare den kompetition som andre skrifttyper fra andre virksomheder såsom “Berthold type foundry”s Akzidenz-grotesk typefaces.</p>
+        <p class="lead"><cite>- Nicolaj, Niklas og Jonas, <a href="https://docs.google.com/document/d/1P3lrHxaTCNIvVE2-MEoYMP3ykz-p0jBsJk38YIdXo2U/edit?usp=sharing" target="_blank">typografi præsentation</a>, 25-9-2020</cite></p>
       </div>
     </div>
-
+    <!-- HELVETICA SLUT -->
+    <hr class="featurette-divider">
+    <!-- TIMES NEW ROMAN -->
+    <div class="row featurette">
+      <div class="col-md-7 order-md-2">
+        <h2 class="featurette-heading">Times new roman.</h2>
+        <p class="lead">Times New Roman er en skrifttype som blev indført i den britiske avis The Times i 1931. Skrifttypen blev designet af Stanley Morison og Victor Lardent. Den blev brugt efter, at Morison havde skrevet en artikel, hvor han kritiserede avisen for den eksisterende serif skrifttype, Didone. Times New Roman er baseret på skrifttypen Plantin, og blev brugt i avisen første gang den 3. oktober 1932. Efter et år blev den lagt ud til kommercielt salg. Avisen brugte skrifttypen i 40 år. Tit skrevet i hånden og i breve & og ligner ofte noget der er skrevet med en pen og blæk, Curved strokes, Thick til thin transitions.</p>
+        <p class="lead"><cite>- Nicolaj, Niklas og Jonas, <a href="https://docs.google.com/document/d/1P3lrHxaTCNIvVE2-MEoYMP3ykz-p0jBsJk38YIdXo2U/edit?usp=sharing" target="_blank">typografi præsentation</a>, 25-9-2020</cite></p>
+      </div>
+    </div>
+    <!-- TIMES NEW ROMAN SLUT -->
     <hr class="featurette-divider">
 
     <!-- /END THE FEATURETTES -->
@@ -108,10 +115,11 @@
       <i class="fab fa-instagram footer-icon"></i>
       <i class="fas fa-envelope footer-icon"></i>
 
-    <p>© Copyright 2020 Jonas Hawthorn Mikkelsen</p>
+    <p class="copyright">© Copyright 2020 Jonas Hawthorn Mikkelsen</p>
   </div>
   </footer>
 </main>
+<!-- JAVASCRIPT BRUGT TIL ALLE UNDERSIDER -->
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
       <script>window.jQuery || document.write('<script src="assets/js/vendor/jquery.slim.min.js"><\/script>')</script><script src="assets/dist/js/bootstrap.bundle.min.js"></script>
 </html>
