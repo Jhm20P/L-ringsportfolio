f4edb38dbf4c2f58faf2e9b5c00a7301aa7bb605
Jhm20P <71630698+Jhm20P@users.noreply.github.com>
2021-01-08 20:07:05 +0100

    Update illustrator.php

--- lærings-portfolio/illustrator.php
+++ lærings-portfolio/illustrator.php
@@ -1,12 +1,13 @@
 <!doctype html>
-<html lang="en">
+<html lang="da">
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
-    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="author" content="Jonas Hawthorn Mikkelsen">
     <meta name="generator" content="Jekyll v4.1.1">
-    <title>Carousel Template · Bootstrap</title>
+    <link rel="icon" href="favicon.ico">
+    <title>Illustrator værktøjer</title>
 
     <link rel="canonical" href="https://getbootstrap.com/docs/4.5/examples/carousel/">
 
@@ -15,9 +16,11 @@
 
     <!-- Bootstrap core CSS -->
 <link href="assets/dist/css/bootstrap.min.css" rel="stylesheet">
+          <!-- EGEN CSS -->
 <link rel="stylesheet" href="underside.css">
 <link rel="stylesheet" href="bodyfix.css">
-
+<link rel="stylesheet" href="billede.css">
+  <!-- INTERN CSS -->
     <style>
       .bd-placeholder-img {
         font-size: 1.125rem;
@@ -37,10 +40,11 @@
     <!-- Custom styles for this template -->
     <link href="carousel.css" rel="stylesheet">
   </head>
+      <!-- MENU BAR -->
   <?php include("Menu.inc"); ?>
 
 <main role="main">
-
+    <!-- BANNER MED OVERSKRIFT OG UNDEROVERSKRIFT -->
   <section class="jumbotron text-center">
     <div class="container">
       <h1>Illustrator værktøjer</h1>
@@ -58,42 +62,41 @@
     <!-- START THE FEATURETTES -->
 
     <hr class="featurette-divider">
-
+    <!-- SELECTOR TOOL -->
     <div class="row featurette">
       <div class="col-md-7">
         <h2 class="featurette-heading">Selectors <span class="text-muted">Vælger et objekt</span></h2>
         <p class="lead">På billede over til højre har vi værktøjskassen, hvor vi har selectors i dette tilfælde. Det man kan bruge selectors til at vælge et objekt at rykke rundt med, eller "group" flere objekter sammen til et objekt. Der er tre forskellige "Selectors" du kan vælge imellem. Det er f.eks. den almindelige "Selection tool", "Direct selection" eller "Group selection". Med "Selection tool" kan du også vælge flere objekter på  samme tid, ved at holde "shift" inde også derefter trykke på det valgte objekt.  Med "Direct selection" kan du ændre formen på det valgte objekt. <cite> -Tony Harmer, Linkedid learning, 20-12-2020 </cite></p>
       </div>
       <div class="col-md-5">
-        <svg  class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 500x500"><image href="select.png" height="500" width="500"/></svg>
+        <image src="select.png" height="auto" width="450"/>
       </div>
     </div>
-
+    <!-- SELECTOR TOOL SLUT -->
     <hr class="featurette-divider">
-
+    <!-- TEGNE VÆRKTØJ -->
     <div class="row featurette">
       <div class="col-md-7 order-md-2">
         <h2 class="featurette-heading">Tegne værktøjer <span class="text-muted">Vælg det der passer bedst</span></h2>
         <p class="lead">Der 6 forskellige værktøjer man kan bruge i værktøjskassen når det kommer til at tegne former, og det kan ses på billedet til venstre. Det første er "Rectangle Tool" som gør at man kan begynde at tegne firkanter. Den næste er hvor man kan tegne firkanter med afrundede kanter. Ved nr 3 kan man tegne cirkler. Ved nr 4 kan man tegne polygoner, såsom femkanter osv. Ved nr 5 kan man tegne stjerner. Og den sidste, kan man tegne sådan noget flare, hvilket kan bruges til f.eks at tegne et øje eller noget andet.<cite> -Tony Harmer, Linkedid learning, 20-12-2020 </cite></p>
       </div>
       <div class="col-md-5 order-md-1">
-        <svg  class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 500x500"><image href="tegn.png" height="500" width="500"/></svg>
+        <image src="tegn.png" height="auto" width="450"/>
       </div>
     </div>
-
+    <!-- TEGNE VÆRKTØJ SLUT -->
     <hr class="featurette-divider">
-
+    <!-- FARVER -->
     <div class="row featurette">
       <div class="col-md-7">
         <h2 class="featurette-heading">Farver. <span class="text-muted">Giv dine objekter en identitet</span></h2>
         <p class="lead">Dette her værktøj som kan ses på billedet til højre,   bruger man til at skifte farve med. Man kan gå ind og vælge den farve man gerne vil, og man kan endda se "hexa" koden for den farve man har valgt sådan at man kan sætte den "hexa" kode ind i sin CSS</p>
       </div>
       <div class="col-md-5">
-        <svg  class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 500x500"><image href="farve.png" height="500" width="500"/></svg>
+        <image src="farve.png" height="auto" width="450"/>
       </div>
     </div>
-
-
+    <!-- FARVER SLUT -->
     <hr class="featurette-divider">
 
     <!-- /END THE FEATURETTES -->
@@ -109,10 +112,11 @@
       <i class="fab fa-instagram footer-icon"></i>
       <i class="fas fa-envelope footer-icon"></i>
 
-    <p>© Copyright 2020 Jonas Hawthorn Mikkelsen</p>
+    <p class="copyright">© Copyright 2020 Jonas Hawthorn Mikkelsen</p>
   </div>
   </footer>
 </main>
+<!-- JAVASCRIPT BRUGT TIL ALLE UNDERSIDER -->
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
       <script>window.jQuery || document.write('<script src="assets/js/vendor/jquery.slim.min.js"><\/script>')</script><script src="assets/dist/js/bootstrap.bundle.min.js"></script>
 </html>
