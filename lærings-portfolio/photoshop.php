e78abe0f69ab10eb0bf4062b1a1272b7038db506
Jhm20P <71630698+Jhm20P@users.noreply.github.com>
2021-01-08 19:09:11 +0100

    Update photoshop.php

--- lærings-portfolio/photoshop.php
+++ lærings-portfolio/photoshop.php
@@ -1,12 +1,13 @@
 <!doctype html>
-<html lang="en">
+<html lang="da">
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
-    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="author" content="Jonas Hawthorn Mikkelsen">
     <meta name="generator" content="Jekyll v4.1.1">
-    <title>Carousel Template · Bootstrap</title>
+    <link rel="icon" href="favicon.ico">
+    <title>Photoshop værktøjer</title>
 
     <link rel="canonical" href="https://getbootstrap.com/docs/4.5/examples/carousel/">
 
@@ -15,9 +16,12 @@
 
     <!-- Bootstrap core CSS -->
 <link href="assets/dist/css/bootstrap.min.css" rel="stylesheet">
+        <!-- EGEN CSS -->
 <link rel="stylesheet" href="underside.css">
 <link rel="stylesheet" href="bodyfix.css">
+<link rel="stylesheet" href="billede.css">
 
+<!-- Intern css -->
     <style>
       .bd-placeholder-img {
         font-size: 1.125rem;
@@ -34,17 +38,20 @@
         }
       }
     </style>
+
     <!-- Custom styles for this template -->
     <link href="carousel.css" rel="stylesheet">
   </head>
+
+  <!-- Menu bar -->
   <?php include("Menu.inc"); ?>
 
 <main role="main">
-
+<!-- Banner med overskrift og underoverskrift -->
   <section class="jumbotron text-center">
     <div class="container">
       <h1>Photoshop værktøjer</h1>
-      <p class="lead text-muted">Her kan man finde informationer omkring hvad jeg har lært angående design</p>
+      <p class="lead text-muted">Her gennegår jeg de værktøjer jeg har brugt mest i Photoshop</p>
     </div>
   </section>
 
@@ -58,41 +65,77 @@
     <!-- START THE FEATURETTES -->
 
     <hr class="featurette-divider">
-
+    <!-- MOVETOOL -->
     <div class="row featurette">
       <div class="col-md-7">
-        <h2 class="featurette-heading">First featurette heading. <span class="text-muted">It’ll blow your mind.</span></h2>
-        <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+        <h2 class="featurette-heading">Move tool. <span class="text-muted">Ryk rundt med dine ting.</span></h2>
+        <p class="lead">"Move tool" er et værktøj du kan bruge til at rykke rundt med dine ting/objekter, den har hotkey'en "v" på dit tastatur. Artboard tool gør at du kan ændre på dine artboads og justere indstillinger på dem.</p>
       </div>
       <div class="col-md-5">
-        <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 500x500"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"/><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg>
+        <image src="movetool.png" height="auto" width="450">
       </div>
     </div>
-
+    <!-- MOVETOOL SLUT -->
     <hr class="featurette-divider">
-
+    <!-- MAGIC WAND TOOL -->
     <div class="row featurette">
       <div class="col-md-7 order-md-2">
-        <h2 class="featurette-heading">Oh yeah, it’s that good. <span class="text-muted">See for yourself.</span></h2>
-        <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+        <h2 class="featurette-heading">Magic wand tool. <span class="text-muted">Markér og slet.</span></h2>
+        <p class="lead">"Magic wand tool" er det tool du kan bruge til at markére et område inde i dit objekt for dig, hvor du herefter kan vælge at slette det der er uden om det valgte område. Med "Quick selection tool" kan man strøje med sin mus henover det man gerne vil have valgt på sit element, hvor efter du kan trykke "Select and Mask" for at lave en maske til netop det man har markéret.</p>
       </div>
       <div class="col-md-5 order-md-1">
-        <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 500x500"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"/><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg>
+        <image src="tryllestav.png" height="auto" width="450">
       </div>
     </div>
-
+    <!-- MAGIC WAND TOOL SLUT -->
     <hr class="featurette-divider">
-
+    <!-- LASSO TOOL -->
     <div class="row featurette">
       <div class="col-md-7">
-        <h2 class="featurette-heading">And lastly, this one. <span class="text-muted">Checkmate.</span></h2>
-        <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+        <h2 class="featurette-heading">Lasso tool. <span class="text-muted">Optejn og slet.</span></h2>
+        <p class="lead">"Lasso tool" fungere på den måde at man kan optegne et område hvorefter man kan slette det område man har optegnet, og dette gøres i frihånd. Forstået på den måde at man kan optejne det lige på den måde man gerne vil, der er ikke nogen form  for "Guidelines". "Polygonal Lasso tool" fungere næsten på samme måde, bare her har du et "guideline" der går at hjælper dig med at optejne i lige linjer. "Magnetic Lasso tool" fungere også næsten på samme måde. Den har så også en form for "Guideline" idet at den føgler omridset af det element du gerne vil tegne op.</p>
       </div>
       <div class="col-md-5">
-        <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 500x500"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"/><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg>
+        <image src="lasso.png" height="auto" width="450">
       </div>
     </div>
-
+    <!-- LASSO TOOL SLUT -->
+    <hr class="featurette-divider">
+    <!-- PAINT BUCKET TOOL -->
+    <div class="row featurette">
+      <div class="col-md-7 order-md-2">
+        <h2 class="featurette-heading">Paint bucket tool. <span class="text-muted">Giv dine elementer en farve.</span></h2>
+        <p class="lead">"Paint bucket tool" kan du bruge til at give et område en farve. Det kan du gøre ved at klikke på værktøjet, vælge en farve også derefter trykke på det område du gerne vil skifte.</p>
+      </div>
+      <div class="col-md-5 order-md-1">
+        <image src="farvespan.png" height="auto" width="450">
+      </div>
+    </div>
+    <!-- PAINT BUCKET TOOL SLUT -->
+    <hr class="featurette-divider">
+    <!-- RECTANGLE TOOL -->
+    <div class="row featurette">
+      <div class="col-md-7">
+        <h2 class="featurette-heading">Rectangle tool. <span class="text-muted">Skab forskellige figurer.</span></h2>
+        <p class="lead">De værktøjer som man kan se over til højre, indeholder blandt andet "Rectangle tool". Med disse værktøjer kan man tegne figurer, såsom firkanter, cirkler, polygoner(femkanter,sekskanter osv.) en lige linje. Dog som man kan se her på billedet, har den ikke den stjerne tool som der er i Illustrator, men man kan stadig tegne den med f.eks. "Polygon tool" og "Costum shape tool".</p>
+      </div>
+      <div class="col-md-5">
+        <image src="formgivning.png" height="auto" width="450">
+      </div>
+    </div>
+    <!-- RECTANGLE TOOL SLUT -->
+    <hr class="featurette-divider">
+    <!-- ZOOM TOOL -->
+    <div class="row featurette">
+      <div class="col-md-7 order-md-2">
+        <h2 class="featurette-heading">Zoom tool. <span class="text-muted">Zomm ind eller ud på dit lærred.</span></h2>
+        <p class="lead">Ved brug af "Zoom tool" kan du zoome ind ved at holde vesntre mussetast inde og træk derefter mod højre. For at zoome ud skal du også holde venstre mussetast inde, men derimod træk mod venstre.</p>
+      </div>
+      <div class="col-md-5 order-md-1">
+        <image src="zoomtool.png" height="auto" width="450">
+      </div>
+    </div>
+    <!-- ZOOM TOOL SLUT -->
     <hr class="featurette-divider">
 
     <!-- /END THE FEATURETTES -->
@@ -108,7 +151,7 @@
       <i class="fab fa-instagram footer-icon"></i>
       <i class="fas fa-envelope footer-icon"></i>
 
-    <p>© Copyright 2020 Jonas Hawthorn Mikkelsen</p>
+    <p class="copyright">© Copyright 2020 Jonas Hawthorn Mikkelsen</p>
   </div>
   </footer>
 </main>
