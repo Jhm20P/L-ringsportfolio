9706d57f7ae0eda0f13a9ea8de083414d58f6c40
Jhm20P <71630698+Jhm20P@users.noreply.github.com>
2021-01-08 23:44:12 +0100

    Update gestaltlovene.php

--- lærings-portfolio/gestaltlovene.php
+++ lærings-portfolio/gestaltlovene.php
@@ -1,12 +1,13 @@
 <!doctype html>
-<html lang="en">
+<html lang="da">
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
-    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="author" content="Jonas Hawthorn Mikkelsen">
     <meta name="generator" content="Jekyll v4.1.1">
-    <title>Carousel Template · Bootstrap</title>
+    <link rel="icon" href="favicon.ico">
+    <title>Gestaltlovene</title>
 
     <link rel="canonical" href="https://getbootstrap.com/docs/4.5/examples/carousel/">
 
@@ -15,9 +16,12 @@
 
     <!-- Bootstrap core CSS -->
 <link href="assets/dist/css/bootstrap.min.css" rel="stylesheet">
+      <!-- EGEN CSS -->
 <link rel="stylesheet" href="underside.css">
 <link rel="stylesheet" href="bodyfix.css">
+<link rel="stylesheet" href="billede.css">
 
+  <!-- INTERN CSS -->
     <style>
       .bd-placeholder-img {
         font-size: 1.125rem;
@@ -37,10 +41,11 @@
     <!-- Custom styles for this template -->
     <link href="carousel.css" rel="stylesheet">
   </head>
+    <!-- MENU BAR -->
   <?php include("Menu.inc"); ?>
 
 <main role="main">
-
+<!-- SEKTION MED OVERSKRIFT OG UNDEROVERSKRIFT -->
   <section class="jumbotron text-center">
     <div class="container">
       <h1>Gestaltlovene</h1>
@@ -58,42 +63,33 @@
     <!-- START THE FEATURETTES -->
 
     <hr class="featurette-divider">
-
+    <!-- GESTALTLOVENE -->
     <div class="row featurette">
       <div class="col-md-7">
-        <h2 class="featurette-heading">First featurette heading. <span class="text-muted">It’ll blow your mind.</span></h2>
-        <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+        <h2 class="featurette-heading">Gestaltlovene.</h2>
+        <p class="lead">Der findes i alt 6 gestaltlove. Den første gestaltlove hedder “Nærhed(proximity)”, som betyder at figurer der er placeret tæt på hinanden ses som gruppe, der har noget relevant til hinanden. Den næste gestaltlov hedder “Lighed(similarity invarians)” er hvor der er tale om ens figurer, disse bliver også set som en grupper der har en relevance til hinanden. Ved den tredje gestaltlov er der tale om “Lukkethed(closure)” og det er hvor delelementer af et billede stykkes sammen til at kunne skabe en helhed. Den fjerde gestaltlov er “Kontinuitet og symmetri” er f.eks. hvor to figurer optræder symmetriske i forhold til hinanden på en linje, vil de blive set som en gruppe, altså at de har en relevance til hinanden.  </p>
+        <p class="lead"><cite><a href="http://informatik-gym.dk/glossary/gestaltlove/" target="_blank">informatik-gym.dk</a>,<time datetime="2020-12-29"> 29-12-2020</time></cite></p>
       </div>
       <div class="col-md-5">
-        <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 500x500"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"/><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg>
+          <image src="design.jpg" height="auto" width="450">
       </div>
     </div>
-
+    <!-- GESTALTLOVENE SLUT -->
     <hr class="featurette-divider">
-
-    <div class="row featurette">
-      <div class="col-md-7 order-md-2">
-        <h2 class="featurette-heading">Oh yeah, it’s that good. <span class="text-muted">See for yourself.</span></h2>
-        <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
-      </div>
-      <div class="col-md-5 order-md-1">
-        <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 500x500"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"/><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg>
-      </div>
-    </div>
-
-    <hr class="featurette-divider">
-
+    <!-- GESTALTLOVENE -->
     <div class="row featurette">
       <div class="col-md-7">
-        <h2 class="featurette-heading">And lastly, this one. <span class="text-muted">Checkmate.</span></h2>
-        <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+        <h2 class="featurette-heading">Gestaltlovene.</h2>
+        <p class="lead">Den femte gestaltlov er “Prægnans(prägnanz). Dette kan man også kalde figur/baggrund. Dette betyder at det er hvor hjernen leder/kigger efter mønstre, kontinuitet og ensartethed. Den sjette gestaltlov er “Erfaring(past experince, Common fate). Dette dækker over brugen af ikoner. Der findes også design tippet “forbundethed” som er f.eks. hvor figurer forbindes med en streg, vil de for det meste blive opfattet som om at de høre sammen. Altså at de har en relevance med hinanden.</p>
+        <p class="lead"><cite><a href="http://informatik-gym.dk/glossary/gestaltlove/" target="_blank">informatik-gym.dk</a>,<time datetime="2020-12-29"> 29-12-2020</time></cite></p>
       </div>
       <div class="col-md-5">
-        <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 500x500"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"/><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg>
+          <image src="design.jpg" height="auto" width="450">
       </div>
     </div>
-
+    <!-- GESTALTLOVENE SLUT -->
     <hr class="featurette-divider">
+
 
     <!-- /END THE FEATURETTES -->
 
@@ -108,10 +104,11 @@
       <i class="fab fa-instagram footer-icon"></i>
       <i class="fas fa-envelope footer-icon"></i>
 
-    <p>© Copyright 2020 Jonas Hawthorn Mikkelsen</p>
+    <p class="copyright">© Copyright 2020 Jonas Hawthorn Mikkelsen</p>
   </div>
   </footer>
 </main>
+<!-- JAVASCRIPT BRUGT TIL ALLE UNDERSIDER -->
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
       <script>window.jQuery || document.write('<script src="assets/js/vendor/jquery.slim.min.js"><\/script>')</script><script src="assets/dist/js/bootstrap.bundle.min.js"></script>
 </html>
