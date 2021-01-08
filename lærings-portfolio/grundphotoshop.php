82af182d3d2a95bf1bc8383e09d0e02b0216c2ab
Jhm20P <71630698+Jhm20P@users.noreply.github.com>
2021-01-08 20:24:43 +0100

    Update grundphotoshop.php

--- lærings-portfolio/grundphotoshop.php
+++ lærings-portfolio/grundphotoshop.php
@@ -4,9 +4,10 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
-    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="author" content="Jonas Hawthorn Mikkelsen">
     <meta name="generator" content="Jekyll v4.1.1">
-    <title>Carousel Template · Bootstrap</title>
+    <link rel="icon" href="favicon.ico">
+    <title>Photoshop</title>
 
     <link rel="canonical" href="https://getbootstrap.com/docs/4.5/examples/carousel/">
 
@@ -15,9 +16,11 @@
 
     <!-- Bootstrap core CSS -->
 <link href="assets/dist/css/bootstrap.min.css" rel="stylesheet">
+        <!-- EGEN CSS -->
 <link rel="stylesheet" href="underside.css">
 <link rel="stylesheet" href="bodyfix.css">
-
+<link rel="stylesheet" href="billede.css">
+  <!-- INTERN CSS -->
     <style>
       .bd-placeholder-img {
         font-size: 1.125rem;
@@ -37,10 +40,11 @@
     <!-- Custom styles for this template -->
     <link href="carousel.css" rel="stylesheet">
   </head>
+    <!-- MENU BAR -->
   <?php include("Menu.inc"); ?>
 
 <main role="main">
-
+  <!-- SEKTION MED OVERSKRIFT OG UNDEROVERSKRIFT -->
   <section class="jumbotron text-center">
     <div class="container">
       <h1>Photoshop</h1>
@@ -58,41 +62,41 @@
     <!-- START THE FEATURETTES -->
 
     <hr class="featurette-divider">
-
+    <!-- CAPTURE FORMATER -->
     <div class="row featurette">
       <div class="col-md-7">
-        <h2 class="featurette-heading">First featurette heading. <span class="text-muted">It’ll blow your mind.</span></h2>
-        <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+        <h2 class="featurette-heading">Capture formater. <span class="sidenavn">Hvad er hvad?</span></h2>
+        <p class="lead">"Jpeg" er en de fil som er mest udbredt, da både computere, mobiler og kameraer kan håndtere dem. Det gode  ved "Jpeg" er at de ikke fylder så meget, dog har det den betydning at det smider meget af det information som filen indeholder væk. Hvis man skal tage billeder vil det være bedst at bruge billede formatet  "Raw". Ved "Raw" beholder man stadig alt det data som ens billede/fil indeholder. Det skal dog siges at "Raw" filen variere lidt efter hvilken producent der har lavet kameraet. Der er også "DNG" filen, som var Adobe's forsøg/svar på at lave form for "Raw" fil der skal kunne benyttes af alle platforme.<cite> -Julieanne Kost, Linkedin learning, 22-12-2020</cite></p>
       </div>
       <div class="col-md-5">
-        <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 500x500"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"/><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg>
+        <image src="fileformat.jpg" height="auto" width="450">
       </div>
     </div>
-
+    <!-- CAPRTURE FORMATER SLUT -->
     <hr class="featurette-divider">
-
+    <!-- WORKING FILES -->
     <div class="row featurette">
       <div class="col-md-7 order-md-2">
-        <h2 class="featurette-heading">Oh yeah, it’s that good. <span class="text-muted">See for yourself.</span></h2>
-        <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+        <h2 class="featurette-heading">Working files. <span class="sidenavn">Hvad betyder de?</span></h2>
+        <p class="lead">"Working files" er brugt når man skal gemme flere lag af dokumenter. Disse dokumenter er filerne "TIFF" eller "PSD". begge af disse filer gemmer alle de valg du har taget i filen, dette indkludere lag, type, former og til sidst justering af lag. Begge filer bruger også en form for komprensision af filen, dog bliver der ikke tabt noget data ved de her to filer.<cite> -Julieanne Kost, Linkedin learning, 22-12-2020</cite></p>
       </div>
       <div class="col-md-5 order-md-1">
-        <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 500x500"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"/><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg>
+        <image src="fileformat.jpg" height="auto" width="450">
       </div>
     </div>
-
+    <!-- WORKING FILES SLUT -->
     <hr class="featurette-divider">
-
+    <!-- OUTPUT FILES -->
     <div class="row featurette">
       <div class="col-md-7">
-        <h2 class="featurette-heading">And lastly, this one. <span class="text-muted">Checkmate.</span></h2>
-        <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+        <h2 class="featurette-heading">Output files.</h2>
+        <p class="lead">Der er "JPEG", "PNG", "GIF", "PDF" samt andre video filer. "JPEG" filen er allerede blevet nævnt om tidligere på siden. Men "PNG" filen er et lag, der understøtter gennemsigtighed, dette betyder at den gør kanterne lidt mere afrunde, samt det er mest brugt til logo'er eller andet flat lignene art. "GIF" filen er også et eneklt lags fil, med max 256 farver som også er kaldet "index color". Selvom "GIF" filen ikke understøtter gennemsigtig lige så godt som en "PNG" fil gør, så kan "GIF" filen derimod lave animationer. Man kan også lave en "PDF" fil. Dette fungere især godt når man har flere forskellige ting man skal vise sin kunde. Video filerne som man eksportere fra Photoshop er: "MOV", "H.264", "DPX".<cite> -Julieanne Kost, Linkedin learning, 22-12-2020</cite></p>
       </div>
       <div class="col-md-5">
-        <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 500x500"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"/><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg>
+        <image src="fileformat.jpg" height="auto" width="450">
       </div>
     </div>
-
+    <!-- OUTPUT FILES SLUT -->
     <hr class="featurette-divider">
 
     <!-- /END THE FEATURETTES -->
@@ -108,10 +112,11 @@
       <i class="fab fa-instagram footer-icon"></i>
       <i class="fas fa-envelope footer-icon"></i>
 
-    <p>© Copyright 2020 Jonas Hawthorn Mikkelsen</p>
+    <p class="copyright">© Copyright 2020 Jonas Hawthorn Mikkelsen</p>
   </div>
   </footer>
 </main>
+<!-- JAVASCRIPT BRUGT TIL ALLE UNDERSIDER -->
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
       <script>window.jQuery || document.write('<script src="assets/js/vendor/jquery.slim.min.js"><\/script>')</script><script src="assets/dist/js/bootstrap.bundle.min.js"></script>
 </html>
