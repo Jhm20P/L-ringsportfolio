80244fdaf6560561322b810dd08a4f33381c0ec3
Jhm20P <71630698+Jhm20P@users.noreply.github.com>
2021-01-09 00:10:05 +0100

    Update quick.php

--- lærings-portfolio/quick.php
+++ lærings-portfolio/quick.php
@@ -1,12 +1,13 @@
 <!doctype html>
-<html lang="en">
+<html lang="da">
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
-    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="author" content="Jonas Hawthorn Mikkelsen">
     <meta name="generator" content="Jekyll v4.1.1">
-    <title>Carousel Template · Bootstrap</title>
+    <link rel="icon" href="favicon.ico">
+    <title>Quick n´ dirty video</title>
 
     <link rel="canonical" href="https://getbootstrap.com/docs/4.5/examples/carousel/">
 
@@ -15,9 +16,12 @@
 
     <!-- Bootstrap core CSS -->
 <link href="assets/dist/css/bootstrap.min.css" rel="stylesheet">
+  <!-- EGEN CSS -->
 <link rel="stylesheet" href="underside.css">
 <link rel="stylesheet" href="bodyfix.css">
-
+<link rel="stylesheet" href="video.css">
+<link rel="stylesheet" href="billede.css">
+  <!-- INTERN CSS -->
     <style>
       .bd-placeholder-img {
         font-size: 1.125rem;
@@ -33,14 +37,17 @@
           font-size: 3.5rem;
         }
       }
+
+
     </style>
     <!-- Custom styles for this template -->
     <link href="carousel.css" rel="stylesheet">
   </head>
+    <!-- MENU BAR -->
   <?php include("Menu.inc"); ?>
 
 <main role="main">
-
+<!-- SEKTION MED OVERSKRIFT OG UNDEROVERSKRIFT -->
   <section class="jumbotron text-center">
     <div class="container">
       <h1>Quick n´ dirty video</h1>
@@ -58,39 +65,48 @@
     <!-- START THE FEATURETTES -->
 
     <hr class="featurette-divider">
-
+    <!-- INDLEDNING -->
     <div class="row featurette">
       <div class="col-md-7">
-        <h2 class="featurette-heading">First featurette heading. <span class="text-muted">It’ll blow your mind.</span></h2>
-        <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+        <h2 class="featurette-heading">Indledning.</h2>
+        <p class="lead">Er et program fra adobe, hvor man kan nemt og hurtigt redigere sine videoer. Programmet er en “light” version af Premier pro, med andre ord, så er Premier rush lillebroren til Premier pro. Premier rush er tilgængelig på Windows, Mac, Android og Apple IOS(App store). Efter at man har importeret sine filer ind i Premiere Rush, kan man køre sin mus hen over filerne, sådan at man markere dem. Der vil herefter komme et tal på hver fil der viser i hvilken rækkefølge dine filer vil blive smidt ind når du skal til at sidde og redigere i det. Man kan også ændre rækkefølgen, ved at trykke på den valgte fil og derefter trykke på den igen.</p>
+        <p class="lead"><cite><a href="https://www.youtube.com/watch?v=Tsc81qbpy6g&t=446s&ab_channel=Storysium" target="_blank">Storysium på Youtube</a>, sidst besøgt <time datetime="2020-12-30">30-12-2020</time></cite></p>
       </div>
       <div class="col-md-5">
-        <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 500x500"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"/><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg>
+        <image class="quick" src="tidslinj.png" height="auto" width="100px">
       </div>
     </div>
-
+    <!-- INDLEDNING SLUT -->
     <hr class="featurette-divider">
-
+    <!-- TIDSLINJEN -->
     <div class="row featurette">
       <div class="col-md-7 order-md-2">
-        <h2 class="featurette-heading">Oh yeah, it’s that good. <span class="text-muted">See for yourself.</span></h2>
-        <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+        <h2 class="featurette-heading">Tidslinjen.</h2>
+        <p class="lead">Nede i venstre hjørne har vi de redskaber man kan bruge til at ændre på tidslinjen i din video, som kan ses på billedet til(højre, venstre.) Saksen  gør sådan at man kan klippe et videoen op i mindre dele, man kan derefter trykke på den del af videoen men også på skraldespanden to som ligger som nummer 3 på billedet. Skraldespanden gør at man sletter det valgte del i videoen. Man kan dublikér sin del i videoen, ved at vælge den del man gerne vil dublikér, og derefter trykke på det værktøj under saksen. Værktøjet under Skraldespanden gør at man kan gøre lyddelen mere synlig på tidslinjen. Hvis man trykker på det nederste værktøj kan man låse, fjerne lyden eller fjerne synligheden på hver videoklip eller hvert video-track på tidslinjen. Hvis man holder “ALT” tasten ned samtidig med at man scroller kan man zoome ind og ud på tidslinjen.</p>
+        <p class="lead"><cite><a href="https://www.youtube.com/watch?v=Tsc81qbpy6g&t=446s&ab_channel=Storysium" target="_blank">Storysium på Youtube</a>, sidst besøgt <time datetime="2020-12-30">30-12-2020</time></cite></p>
       </div>
       <div class="col-md-5 order-md-1">
-        <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 500x500"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"/><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg>
+        <image class="quick" src="tidslinj.png" height="auto" width="100px">
       </div>
     </div>
-
+    <!-- TIDSLINJEN SLUT -->
     <hr class="featurette-divider">
-
+    <!-- DET VISUELLE -->
     <div class="row featurette">
       <div class="col-md-7">
-        <h2 class="featurette-heading">And lastly, this one. <span class="text-muted">Checkmate.</span></h2>
-        <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+        <h2 class="featurette-heading">Det visuelle.</h2>
+        <p class="lead">Ved værktøjerne over i højre side har vi de værktøj der kan justere på det visuelle i din video. Det første værktøj er “Tekstværktøjet” Her kan man sætte en tekst ind på videoen. Det næste værktøj lige under er “Transitioner” her kan man give et klip en transition samt ændre hvor lang den transition skal være. Det tredje værktøj er hvor man kan ændre farven på det valgte del af videoen nede i tidslinjen. Det fjerde værktøj, er hvor du kan ændre på hastigheden af det valgte del i tidslinjen. Og man kan endda bestemme hvor meget af den del i tidslinjen der skal påvirkes. Det femte værktøj, er hvor man kan ændre lydstyrken på det valgte del i tidslinjen, samt det at man kan vælge om det skal være stemmer, baggrundslyd, og musikken. Det sidste værktøj, er hvor man kan bestemme størrelsen på det valgte del af videoen i tidslinjen.</p>
+        <p class="lead"><cite><a href="https://www.youtube.com/watch?v=Tsc81qbpy6g&t=446s&ab_channel=Storysium" target="_blank">Storysium på Youtube</a>, sidst besøgt <time datetime="2020-12-30">30-12-2020</time></cite></p>
       </div>
       <div class="col-md-5">
-        <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 500x500"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"/><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg>
+        <image class="quick" src="visuelle.png" height="auto" width="100px">
       </div>
+    </div>
+    <!-- DET VISUELLE SLUT -->
+    <hr class="featurette-divider">
+    <!-- YOUTUBE GRUPPEARBEJDE VIDEO -->
+    <div class="embed">
+    <iframe class="quick-video" width="1110" height="624" src="https://www.youtube.com/embed/EZBNnUBNN8A" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     </div>
 
     <hr class="featurette-divider">
@@ -108,10 +124,11 @@
       <i class="fab fa-instagram footer-icon"></i>
       <i class="fas fa-envelope footer-icon"></i>
 
-    <p>© Copyright 2020 Jonas Hawthorn Mikkelsen</p>
+    <p class="copyright">© Copyright 2020 Jonas Hawthorn Mikkelsen</p>
   </div>
   </footer>
 </main>
+<!-- JAVASCRIPT BRUGT TIL ALLE UNDERSIDER -->
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
       <script>window.jQuery || document.write('<script src="assets/js/vendor/jquery.slim.min.js"><\/script>')</script><script src="assets/dist/js/bootstrap.bundle.min.js"></script>
 </html>
