2db59cf42f91b0687a0ab5fb6cedc047954e9a6f
Jhm20P <71630698+Jhm20P@users.noreply.github.com>
2021-01-08 21:13:03 +0100

    Update farveteori.php

--- lærings-portfolio/farveteori.php
+++ lærings-portfolio/farveteori.php
@@ -1,12 +1,13 @@
 <!doctype html>
-<html lang="en">
+<html lang="da">
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
-    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="author" content="Jonas Hawthorn Mikkelsen">
     <meta name="generator" content="Jekyll v4.1.1">
-    <title>Carousel Template · Bootstrap</title>
+    <link rel="icon" href="favicon.ico">
+    <title>Farveteori</title>
 
     <link rel="canonical" href="https://getbootstrap.com/docs/4.5/examples/carousel/">
 
@@ -15,9 +16,11 @@
 
     <!-- Bootstrap core CSS -->
 <link href="assets/dist/css/bootstrap.min.css" rel="stylesheet">
+      <!-- EGEN CSS -->
 <link rel="stylesheet" href="underside.css">
 <link rel="stylesheet" href="bodyfix.css">
-
+<link rel="stylesheet" href="billede.css">
+  <!-- INTERN CSS -->
     <style>
       .bd-placeholder-img {
         font-size: 1.125rem;
@@ -40,10 +43,10 @@
   <?php include("Menu.inc"); ?>
 
 <main role="main">
-
+<!-- SEKTION MED OVERSKRIFT OG UNDEROVERSKRIFT -->
   <section class="jumbotron text-center">
     <div class="container">
-      <h1>farveteori</h1>
+      <h1>Farveteori</h1>
       <p class="lead text-muted">Her kan man finde informationer omkring hvad jeg har lært angående farveteori</p>
     </div>
   </section>
@@ -58,42 +61,65 @@
     <!-- START THE FEATURETTES -->
 
     <hr class="featurette-divider">
-
+    <!-- FARVEPSYKOLOGI -->
     <div class="row featurette">
       <div class="col-md-7">
-        <h2 class="featurette-heading">First featurette heading. <span class="text-muted">It’ll blow your mind.</span></h2>
-        <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+        <h2 class="featurette-heading">Farvepsykologi. <span class="sidenavn">It’ll blow your mind.</span></h2>
+        <p class="lead">Farve have ikke den helt store betydning før 1810 da det var der man kom med den første bog omkring farver.
+Men der har dog først været omkring 1960'erne man blev mere bevidst omkring farver, da vi begyndte at få muligheder for mere salg via. transport til andre lande. Apel former har også en rolle når vi snakker om farver, da det kommer helt an på hvilket emne der bliver snakket om i ens reklame f.eks. Det kan være fra redbarnet, her vil man helst bruge en rød og en hvid farve. da rød kan symbolisere kærlighed og omsorg, hvor hvid kan for en til at føle sig fokuseret.</p>
+        <p class="lead"><cite>- Jonas, Moodle, 24 sep, 2020</cite></p>
       </div>
       <div class="col-md-5">
-        <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 500x500"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"/><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg>
+        <image src="farvepsyk.jpg" height="auto" width="450">
       </div>
     </div>
-
+    <!-- FARVEPSYKOLOGI SLUT -->
     <hr class="featurette-divider">
-
+    <!-- FARVETOERI -->
     <div class="row featurette">
       <div class="col-md-7 order-md-2">
-        <h2 class="featurette-heading">Oh yeah, it’s that good. <span class="text-muted">See for yourself.</span></h2>
-        <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+        <h2 class="featurette-heading">Farveteori. <span class="sidenavn">See for yourself.</span></h2>
+        <p class="lead">Farver kan man bruge til mange ting, til at male sit hus, plakat, eller man kan også bruge farver på sin hjemmeside. Det som farver gør, er at give en genstand en identitet som den ikke havde før uden farver. Men når man skal til tage valg over hvilke farver man gerne vil bruge, skal man være varsom, da farver kan have flere betydninger, samt sammensætningen af farver. Komplementære kontraster er to farver der står over for hinanden på farvecirklen. Monochromatic er hvor det kun er en farve. Analogous er hvor det tre farver der er lige placeret lige ved siden af hinanden på farvecirklen. Triadic er tre farver der har lige meget afstand mellem dem på farvercirklen.  Split komplementær er lidt lige som komplementær, men bare hvor en af enderne er spredt ud til to mere farver på farvecirklen. Tetratic er hvor det er to par af modsatte farver på farvecirklen. Med andre ord er det en dobbelte komplementær kontrast.</p>
+        <p class="lead"><cite>- Jonas Hawthorn Mikkelsen,<a href="https://docs.google.com/document/d/1wfzXHlKZJXarBYpLw_Xdxjsc9J42_WNaitZghqvT2_Y/edit?usp=sharing"> Kom/it HTX</a>, 14-5-2018, Blender Guru <a href="https://www.youtube.com/watch?v=Qj1FK8n7WgY&feature=youtu.be&ab_channel=BlenderGuru">Youtube</a>, 27 Feburar 2014</cite></p>
       </div>
       <div class="col-md-5 order-md-1">
-        <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 500x500"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"/><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg>
+        <image src="farveteori.jpg" height="auto" width="450">
       </div>
     </div>
-
+    <!-- FARVETOERI SLUT -->
     <hr class="featurette-divider">
-
+    <!-- COCA COLA -->
     <div class="row featurette">
       <div class="col-md-7">
-        <h2 class="featurette-heading">And lastly, this one. <span class="text-muted">Checkmate.</span></h2>
-        <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+        <h2 class="featurette-heading">CocaCola.</h2>
+        <p class="lead">-Billederne er alle med noget rødt i (en analoge farvesammensætning).</p>
+        <p class="lead">Den røde farve i logoet symboliserer dominans, kærlighed. Coca Cola vil gerne være dominerende på markedet og vil gerne have at kunder asscierer mærket med kærlighed.</p>
+        <p class="lead">-Tekst: Primært sort på hvid baggrund (sort/hvid-kontrast, tidløst) eller hvid på rød baggrund. Begge valg er letlæselige.</p>
+        <p class="lead"><cite>- Bjarne, Maja og Jonas, Moodle, 24 sep, 2020</cite></p>
       </div>
       <div class="col-md-5">
-        <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 500x500"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"/><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg>
+        <image src="cola.jpg" height="auto" width="450">
       </div>
     </div>
-
+    <!-- COCA COLA SLUT -->
     <hr class="featurette-divider">
+    <!-- MCDONALDS -->
+    <div class="row featurette">
+      <div class="col-md-7 order-md-2">
+        <h2 class="featurette-heading">McDonalds.</h2>
+        <p class="lead">-Call to actions-knapper er konsekvent gule ligesom M´et i deres logo. Den gule er valgt fordi den er mindre dominerende end den røde i logoet.</p>
+        <p class="lead">-Meget tekst er rød, specielt under familie-sektionen (barnligt).</p>
+        <p class="lead">-En analog farvesammensætning omkring røde farver.</p>
+        <p class="lead">-De gule, røde og orange farver symboliserer her: varme, hygge, sjov og energi.</p>
+        <p class="lead"><cite>- Bjarne, Maja og Jonas, Moodle, 24 sep, 2020</cite></p>
+      </div>
+      <div class="col-md-5 order-md-1">
+        <image src="mcd.jpg" height="auto" width="450">
+      </div>
+    </div>
+    <!-- MCDONALDS SLUT -->
+    <hr class="featurette-divider">
+
 
     <!-- /END THE FEATURETTES -->
 
@@ -108,10 +134,11 @@
       <i class="fab fa-instagram footer-icon"></i>
       <i class="fas fa-envelope footer-icon"></i>
 
-    <p>© Copyright 2020 Jonas Hawthorn Mikkelsen</p>
+    <p class="copyright">© Copyright 2020 Jonas Hawthorn Mikkelsen</p>
   </div>
   </footer>
 </main>
+<!-- JAVASCRIPT BRUGT TIL ALLE UNDERSIDER -->
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
       <script>window.jQuery || document.write('<script src="assets/js/vendor/jquery.slim.min.js"><\/script>')</script><script src="assets/dist/js/bootstrap.bundle.min.js"></script>
 </html>
