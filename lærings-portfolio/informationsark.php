e5736a6578e0dc83bb87e65d6878286f3f6a09f4
Jhm20P <71630698+Jhm20P@users.noreply.github.com>
2021-01-09 18:09:05 +0100

    Update informationsark.php

--- lærings-portfolio/informationsark.php
+++ lærings-portfolio/informationsark.php
@@ -1,12 +1,13 @@
 <!doctype html>
-<html lang="en">
+<html lang="da">
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
-    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="author" content="Jonas Hawthorn Mikkelsen">
     <meta name="generator" content="Jekyll v4.1.1">
-    <title>Carousel Template · Bootstrap</title>
+    <link rel="icon" href="favicon.ico">
+    <title>Informationsarkitektur</title>
 
     <link rel="canonical" href="https://getbootstrap.com/docs/4.5/examples/carousel/">
 
@@ -15,9 +16,10 @@
 
     <!-- Bootstrap core CSS -->
 <link href="assets/dist/css/bootstrap.min.css" rel="stylesheet">
+  <!-- EGEN CSS -->
 <link rel="stylesheet" href="underside.css">
 <link rel="stylesheet" href="bodyfix.css">
-
+  <!-- INTERN CSS -->
     <style>
       .bd-placeholder-img {
         font-size: 1.125rem;
@@ -37,14 +39,15 @@
     <!-- Custom styles for this template -->
     <link href="carousel.css" rel="stylesheet">
   </head>
+  <!-- MENU BAR -->
   <?php include("Menu.inc"); ?>
 
 <main role="main">
-
+<!-- SEKTION MED OVERSKRIFT OG UNDEROVERSKRIFT -->
   <section class="jumbotron text-center">
     <div class="container">
       <h1>Informationsarkitektur</h1>
-      <p class="lead text-muted">Her kan man finde informationer omkring hvad jeg har lært angående design</p>
+      <p class="lead text-muted">Her kan man finde informationer omkring hvad jeg har lært  angående informationsarkitektur</p>
     </div>
   </section>
 
@@ -58,41 +61,43 @@
     <!-- START THE FEATURETTES -->
 
     <hr class="featurette-divider">
-
+    <!-- INFORMATIONSARKITEKTUR -->
     <div class="row featurette">
       <div class="col-md-7">
-        <h2 class="featurette-heading">First featurette heading. <span class="text-muted">It’ll blow your mind.</span></h2>
-        <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
-      </div>
-      <div class="col-md-5">
-        <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 500x500"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"/><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg>
+        <h2 class="featurette-heading">Informationsarkitektur.</h2>
+        <p class="lead"> Informationsarkitektur er hvor man organisere sine ting, hvilket også gælder på hjemmesider. Test af informationsarkitektur kan laves ved brug af kortsortering.  Ved kortsortering kan man både få ens modtager til at designe sit informationsarkitektur, men man kan også komme med navne på tingene til sin hjemmeside, hvor man vil teste det på en modtager, for at få dem til at organisere informationsarkitekturen på hjemmesiden. Jo flere du tester det på, jo bedre, da du vil kunne komme frem til et mere generel enighed fra dine modtagere. Der fem måde man kan organisere sin ting på, og det er ved at gøre brug af “LATCH” modellen. “L” står for lokation, “A” står for alfabetet, “T” står for tid, “c” står for kategori og “H” står for hierarki.</p>
+        <p class="lead">LATCH modellen er beskrevet her under:</p>
+        <ul>
+          <li>L - Lokation
+            <ul>
+              <li>Kan være mange forskellige måder man kan organisere sine ting efter lokation. Det kan f.eks. være at man sætter den bestemte del i menuen i en rækkefølge der passer til hvordan de er placeret på en af undersiderne.</li>
+            </ul>
+          </li>
+          <li>A - Alfabetet
+            <ul>
+              <li>Det vil sige at man organisere sin menu f.eks. efter alfabetisk rækkefølge. Typiske steder man kunne se sådan en alfabetisk orden, var i telefonbøger.</li>
+            </ul>
+          </li>
+          <li>T - Tid
+            <ul>
+              <li>Her vil du organisere efter tid. Det kan man f.eks. gøre ved at organisere dine ting efter hvornår du laver, hvilket tid den kilde der passer til din tekst blev skrevet.</li>
+            </ul>
+          </li>
+          <li>C - Kategori
+            <ul>
+              <li>Her vil man organisere sine ting ved at placere de ting der i samme fag, eller går ind omkring det samme område, vil man sætte i samme boks f.eks.</li>
+            </ul>
+          </li>
+          <li>H - Hierarki
+            <ul>
+              <li>Når man skal organisere sine ting i hierakisk rækkefølge, kan man tage det emne der dækker mere grundlæggende over det hele/grundstenene inden for det område eller man kan f.eks. også organisere efter hvad der er vigtigst, det vil sige at det der er vigtigst kommer først.</li>
+            </ul>
+          </li>
+        </ul>
+        <p class="lead"><cite>-Jonas, Moodle/Niel's <a class="google" href="informationsArkitektur.html" target="_blank">powerpoint</a>, <time datetime="2020-11-10">10 November, 2020</time></cite></p>
       </div>
     </div>
-
-    <hr class="featurette-divider">
-
-    <div class="row featurette">
-      <div class="col-md-7 order-md-2">
-        <h2 class="featurette-heading">Oh yeah, it’s that good. <span class="text-muted">See for yourself.</span></h2>
-        <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
-      </div>
-      <div class="col-md-5 order-md-1">
-        <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 500x500"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"/><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg>
-      </div>
-    </div>
-
-    <hr class="featurette-divider">
-
-    <div class="row featurette">
-      <div class="col-md-7">
-        <h2 class="featurette-heading">And lastly, this one. <span class="text-muted">Checkmate.</span></h2>
-        <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
-      </div>
-      <div class="col-md-5">
-        <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 500x500"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"/><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg>
-      </div>
-    </div>
-
+    <!-- INFORMATIONSARKITEKTUR SLUT -->
     <hr class="featurette-divider">
 
     <!-- /END THE FEATURETTES -->
@@ -108,10 +113,11 @@
       <i class="fab fa-instagram footer-icon"></i>
       <i class="fas fa-envelope footer-icon"></i>
 
-    <p>© Copyright 2020 Jonas Hawthorn Mikkelsen</p>
+    <p class="copyright">© Copyright 2020 Jonas Hawthorn Mikkelsen</p>
   </div>
   </footer>
 </main>
+<!-- JAVASCRIPT BRUGT TIL ALLE UNDERSIDER -->
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
       <script>window.jQuery || document.write('<script src="assets/js/vendor/jquery.slim.min.js"><\/script>')</script><script src="assets/dist/js/bootstrap.bundle.min.js"></script>
 </html>
