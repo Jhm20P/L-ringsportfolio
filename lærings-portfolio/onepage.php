a088ce04cc49c16e78d936870216f149de253f3b
Jhm20P <71630698+Jhm20P@users.noreply.github.com>
2021-01-09 18:41:44 +0100

    Update onepage.php

--- lærings-portfolio/onepage.php
+++ lærings-portfolio/onepage.php
@@ -1,12 +1,13 @@
 <!doctype html>
-<html lang="en">
+<html lang="da">
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
-    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="author" content="Jonas Hawthorn Mikkelsen">
     <meta name="generator" content="Jekyll v4.1.1">
-    <title>Carousel Template · Bootstrap</title>
+    <link rel="icon" href="favicon.ico">
+    <title>Onepage</title>
 
     <link rel="canonical" href="https://getbootstrap.com/docs/4.5/examples/carousel/">
 
@@ -15,9 +16,11 @@
 
     <!-- Bootstrap core CSS -->
 <link href="assets/dist/css/bootstrap.min.css" rel="stylesheet">
+  <!-- EGEN CSS -->
 <link rel="stylesheet" href="underside.css">
 <link rel="stylesheet" href="bodyfix.css">
-
+<link rel="stylesheet" href="billede.css">
+  <!-- INTERN CSS -->
     <style>
       .bd-placeholder-img {
         font-size: 1.125rem;
@@ -37,14 +40,15 @@
     <!-- Custom styles for this template -->
     <link href="carousel.css" rel="stylesheet">
   </head>
+  <!-- MENU BAR -->
   <?php include("Menu.inc"); ?>
 
 <main role="main">
-
+<!-- SEKTION MED OVERSKRIFT OG UNDEROVERSKRIFT -->
   <section class="jumbotron text-center">
     <div class="container">
       <h1>Onepage</h1>
-      <p class="lead text-muted">Her kan man finde informationer omkring hvad jeg har lært angående design</p>
+      <p class="lead text-muted">Her kan man finde informationer omkring hvad vi som gruppe lave til Onepage projektet</p>
     </div>
   </section>
 
@@ -58,41 +62,91 @@
     <!-- START THE FEATURETTES -->
 
     <hr class="featurette-divider">
-
+    <!-- INDLEDNING -->
     <div class="row featurette">
       <div class="col-md-7">
-        <h2 class="featurette-heading">First featurette heading. <span class="text-muted">It’ll blow your mind.</span></h2>
-        <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
-      </div>
-      <div class="col-md-5">
-        <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 500x500"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"/><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg>
+        <h2 class="featurette-heading">Indledning.</h2>
+        <p class="lead">I det her onepage projekt skulle vi lave en hjemmeside/onepage over et bestemt produkt. Produktet vi fik var skosværte, fra mærket "Silver". Så her under Indledningen vil der står vores arbejdsprocess samt vores produkter.</p>
       </div>
     </div>
-
+    <!-- INDLEDNING SLUT -->
     <hr class="featurette-divider">
-
+    <!-- EMPATHIZE -->
     <div class="row featurette">
       <div class="col-md-7 order-md-2">
-        <h2 class="featurette-heading">Oh yeah, it’s that good. <span class="text-muted">See for yourself.</span></h2>
-        <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+        <h2 class="featurette-heading">Onepage. <span class="sidenavn">Empathize.</span></h2>
+        <p class="lead">Det første vi lavede var en en research oversigt over virksomheden "Silver" som var dem der har produceret den skosværte vi fik.</p>
+        <p class="lead"><cite>-Terkel, Sine og Jonas, Moodle/<a class="google" href="https://docs.google.com/document/d/1AC7nCyhnkEZyyxV6ES8CdYuCzKk2DwkUlzRiJi-wxXE/edit?usp=sharing" target="_blank">Google docs</a>, <time datetime="2020-11-09">9 November, 2020</time></cite></p>
       </div>
       <div class="col-md-5 order-md-1">
-        <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 500x500"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"/><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg>
+        <img src="silver.png" alt="">
       </div>
     </div>
-
+    <!-- EMPATHIZE SLUT -->
     <hr class="featurette-divider">
-
+    <!-- IDEATE -->
     <div class="row featurette">
       <div class="col-md-7">
-        <h2 class="featurette-heading">And lastly, this one. <span class="text-muted">Checkmate.</span></h2>
-        <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+        <h2 class="featurette-heading">Onepage. <span class="sidenavn">Ideate.</span></h2>
+        <p class="lead">Det er så her vi begyndte med at brainstorme alt hvad vores onepage skulle have i forhold til vores produkt. Igen brugte vi en tavle til at skrive alle vores ideér ned på.</p>
+        <p class="lead"><cite>-Terkel, Sine og Jonas, Moodle/<a class="google" href="https://docs.google.com/document/d/1AC7nCyhnkEZyyxV6ES8CdYuCzKk2DwkUlzRiJi-wxXE/edit?usp=sharing" target="_blank">Google docs</a>, <time datetime="2020-11-10">10 November, 2020</time></cite></p>
       </div>
       <div class="col-md-5">
-        <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 500x500"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"/><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg>
+        <img src="skopleje.png" alt="">
       </div>
     </div>
-
+    <!-- IDEATE SLUT -->
+    <hr class="featurette-divider">
+    <!-- PROTOTYPE OG TEST -->
+    <div class="row featurette">
+      <div class="col-md-7 order-md-2">
+        <h2 class="featurette-heading">Onepage. <span class="sidenavn">Prototype og test.</span></h2>
+        <p class="lead">Her begyndte vi med at skitsere vores onepage med wireframes både for mobil, tablet og desktop. Når vi var færdige med den første version, gik vi ud testede. Efter test lavede vi ændringer i forhold til det feedback vi havde fået, og sådan gjorde vi 3 gange.</p>
+        <p class="lead">sidste prototype mobil</p>
+        <img src="2mobilproto.png" alt="">
+        <p class="">sidste prototype tablet</p>
+        <img src="2tabletproto.png" alt="">
+        <p class="lead">sidste prototype desktop</p>
+        <img src="2desktopproto.png" alt="">
+        <p class="lead"><cite>-Terkel, Sine og Jonas, Moodle/<a class="google" href="https://docs.google.com/document/d/1AC7nCyhnkEZyyxV6ES8CdYuCzKk2DwkUlzRiJi-wxXE/edit?usp=sharing" target="_blank">Google docs</a>, <time datetime="2020-11-10">10 November, 2020</time></cite></p>
+      </div>
+      <div class="col-md-5 order-md-1">
+        <p class="lead">Første prototype mobil</p>
+        <img src="1mobilproto.png" alt="">
+        <p class="">Første prototype tablet</p>
+        <img src="1tabletproto.png" alt="">
+        <p class="lead">Første prototype desktop</p>
+        <img src="1desktopproto.png" alt="">
+      </div>
+    </div>
+    <!-- PROTOTYPE OG TEST SLUT -->
+    <hr class="featurette-divider">
+    <!-- HIGH FIDELITY -->
+    <div class="row featurette">
+      <div class="col-md-7">
+        <h2 class="featurette-heading">Onepage. <span class="sidenavn">High fidelity.</span></h2>
+        <p class="lead">Til vores high fidelity lavede vi først et styletile, der ligesom skulle kort og præcis forklare hvilke skrifttyper der skulle bruges, hvor de skulle bruges. Hvilke farver der skulle bruges samt hvor de skulle bruges. I styletile har vi også vist et billede eksempel som vi skulle bruge på vores onepage. Men når vi var færdige med at lave det her styletile kunne vi gå ud og lave vores high fidelity.</p>
+        <p class="lead">Du kan finde high fidelity for mobilen <a href="https://xd.adobe.com/view/7f0c0e3c-36c0-486b-b83e-59514aa23014-2a5f/?fbclid=IwAR2LXf22g36-k2ygXkK6v9Cr0qFZNpRrVr_ecV5ExkOa1fTDLf5rXRUT-VU" target="_blank">her</a>.</p>
+        <p class="lead">Du kan finde high fidelity for tablet <a href="https://xd.adobe.com/view/d4519a7b-cec9-47f2-9cb3-99c25b859fd2-8102/?fbclid=IwAR2jsOIAZ959Wk3MfAvdmMqnxnUF3noFbg_XyVMXP8-iEw2612qFjgU_WnE" target="_blank">her</a>.</p>
+        <p class="lead">Du kan finde high fidelity for desktopen <a href="https://xd.adobe.com/view/f202454e-6980-4c00-b114-9afa2e306a1f-a751/?fbclid=IwAR0rVjEK5g-asbFDhmGuH_8YNifQNh2j3c3JCfcCHhP-XuCyMXVi51sQzJM" target="_blank">her</a>.</p>
+        <p class="lead"><cite>-Terkel, Sine og Jonas, Moodle/<a class="google" href="https://docs.google.com/document/d/1AC7nCyhnkEZyyxV6ES8CdYuCzKk2DwkUlzRiJi-wxXE/edit?usp=sharing" target="_blank">Google docs</a>, <time datetime="2020-11-13">11-13 November, 2020</time></cite></p>
+      </div>
+      <div class="col-md-5">
+        <img src="styletile.png" alt="">
+      </div>
+    </div>
+    <!-- HIGH FIDELITY SLUT -->
+    <hr class="featurette-divider">
+    <!-- IMPLEMENTERING -->
+    <div class="row featurette">
+      <div class="col-md-7">
+        <h2 class="featurette-heading">Onepage. <span class="sidenavn">Implementering.</span></h2>
+        <p class="lead">I vores Implementering fase skulle vi kode vores onepage efter hvordan vi har haft den på vores high fidelity eksempler. Efter at vi havde kodet vores onepage gik vi rundt og testede onepage, ved at spørge folk(klassekamerater) hvad de synes om vores onepage, og hvad vi kunne gøre bedre ved den.</p>
+        <p class="lead">Vi endte med at få en onepage der kom til at sådan her ud, og du kan finde linket <a href="http://10741.apache.eadania.dk/onepage/index.html?fbclid=IwAR0TXWrmj_-SuQe0uBUK4Uy2vQE4vkHVHBjnxt2g27E5Hw0JvMh7SxhqsqA" target="_blank">her</a>.</p>
+        <p class="lead"><cite>-Terkel, Sine og Jonas, Moodle/<a class="google" href="https://docs.google.com/document/d/1AC7nCyhnkEZyyxV6ES8CdYuCzKk2DwkUlzRiJi-wxXE/edit?usp=sharing" target="_blank">Google docs</a>, <time datetime="2020-11-19">16-19 November, 2020</time></cite></p>
+      </div>
+    </div>
+    <!-- IMPLEMENTERING SLUT -->
     <hr class="featurette-divider">
 
     <!-- /END THE FEATURETTES -->
@@ -108,10 +162,11 @@
       <i class="fab fa-instagram footer-icon"></i>
       <i class="fas fa-envelope footer-icon"></i>
 
-    <p>© Copyright 2020 Jonas Hawthorn Mikkelsen</p>
+    <p class="copyright">© Copyright 2020 Jonas Hawthorn Mikkelsen</p>
   </div>
   </footer>
 </main>
+<!-- JAVASCRIPT BRUGT TIL ALLE UNDERSIDER -->
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
       <script>window.jQuery || document.write('<script src="assets/js/vendor/jquery.slim.min.js"><\/script>')</script><script src="assets/dist/js/bootstrap.bundle.min.js"></script>
 </html>
