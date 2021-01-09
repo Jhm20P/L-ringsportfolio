4f08e09040ebd43dcb00fe11a09cf74c907b297c
Jhm20P <71630698+Jhm20P@users.noreply.github.com>
2021-01-09 19:01:31 +0100

    Update design.php

--- lærings-portfolio/design.php
+++ lærings-portfolio/design.php
@@ -1,12 +1,13 @@
 <!doctype html>
-<html lang="en">
+<html lang="da">
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
-    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="author" content="Jonas Hawthorn Mikkelsen">
     <meta name="generator" content="Jekyll v4.1.1">
-    <title>Album example · Bootstrap</title>
+    <link rel="icon" href="favicon.ico">
+    <title>Design</title>
 
     <!-- Javascript for iconer fra fontawesome -->
     <script src="https://kit.fontawesome.com/c43d39253a.js" crossorigin="anonymous"></script>
@@ -15,8 +16,9 @@
 
     <!-- Bootstrap core CSS -->
 <link href="assets/dist/css/bootstrap.min.css" rel="stylesheet">
+<!-- EGEN CSS -->
 <link rel="stylesheet" href="underside.css">
-
+  <!-- INTERN CSS -->
     <style>
       .bd-placeholder-img {
         font-size: 1.125rem;
@@ -34,13 +36,14 @@
       }
     </style>
     <!-- Custom styles for this template -->
+    <!-- MENU BAR -->
     <?php include("Menu.inc"); ?>
 
 <main role="main">
-
+<!-- SEKTION MED OVERSKRIFT OG UNDEROVERSKRIFT -->
   <section class="jumbotron text-center">
     <div class="container">
-      <h1>Design</h1>
+      <h1 class="text-muted">Design</h1>
       <p class="lead text-muted">Her kan man finde informationer omkring hvad jeg har lært angående design</p>
     </div>
   </section>
@@ -49,9 +52,10 @@
     <div class="container">
 
       <div class="row">
+        <!-- PHOTOSHOP VÆRKTØJ -->
         <div class="col-md-4">
           <div class="card mb-4 shadow-sm">
-            <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+            <img class="card-img-top" src="photværk.png" width="100%" height="225">
             <div class="card-body">
               <p class="card-text">Denne her indeholder fakta og viden omkring værktøjerne i Photoshop</p>
               <div class="d-flex justify-content-between align-items-center">
@@ -62,9 +66,10 @@
             </div>
           </div>
         </div>
+        <!-- ILLUSTRATOR VÆRKTØJ -->
         <div class="col-md-4">
           <div class="card mb-4 shadow-sm">
-            <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+          <img class="card-img-top" src="iluværk.png" width="100%" height="225">
             <div class="card-body">
               <p class="card-text">Denne her indeholder fakta og viden omkring værktøjerne i Illustrator</p>
               <div class="d-flex justify-content-between align-items-center">
@@ -75,9 +80,10 @@
             </div>
           </div>
         </div>
+        <!-- PHOTOSHOP GRUNDFORLØBET -->
         <div class="col-md-4">
           <div class="card mb-4 shadow-sm">
-            <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+            <img class="card-img-top" src="photgrund.png" width="100%" height="225">
             <div class="card-body">
               <p class="card-text">Denne her indeholder hvad jeg har lavet i Photoshop under grundforløbet</p>
               <div class="d-flex justify-content-between align-items-center">
@@ -88,10 +94,10 @@
             </div>
           </div>
         </div>
-
+        <!-- ILLUSTRATOR GRUNDFORLØBET -->
         <div class="col-md-4">
           <div class="card mb-4 shadow-sm">
-            <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+            <img class="card-img-top" src="illugrund.png" width="100%" height="225">
             <div class="card-body">
               <p class="card-text">Denne her indeholder hvad jeg har lavet i Illustrator under grundforløbet</p>
               <div class="d-flex justify-content-between align-items-center">
@@ -102,9 +108,10 @@
             </div>
           </div>
         </div>
+        <!-- FARVETEORI -->
         <div class="col-md-4">
           <div class="card mb-4 shadow-sm">
-            <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+            <img class="card-img-top" src="farvetr.png" width="100%" height="225">
             <div class="card-body">
               <p class="card-text">Denne her indeholder fakta og viswn omkring farveteori</p>
               <div class="d-flex justify-content-between align-items-center">
@@ -115,9 +122,10 @@
             </div>
           </div>
         </div>
+        <!-- TYPOGRAFI -->
         <div class="col-md-4">
           <div class="card mb-4 shadow-sm">
-            <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+            <img class="card-img-top" src="typografi.png" width="100%" height="225">
             <div class="card-body">
               <p class="card-text">Denne her indeholder fakta og viden omkring typografi</p>
               <div class="d-flex justify-content-between align-items-center">
@@ -128,10 +136,10 @@
             </div>
           </div>
         </div>
-
+        <!-- GESTALTLOVENE -->
         <div class="col-md-4">
           <div class="card mb-4 shadow-sm">
-            <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+            <img class="card-img-top" src="gestaltloven.png" width="100%" height="225">
             <div class="card-body">
               <p class="card-text">Denne her indeholder fakta og viden omkring Gestaltlovene</p>
               <div class="d-flex justify-content-between align-items-center">
@@ -142,9 +150,10 @@
             </div>
           </div>
         </div>
+        <!-- QUICK'N DIRTY VIDEO -->
         <div class="col-md-4">
           <div class="card mb-4 shadow-sm">
-            <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+            <img class="card-img-top" src="quick.png" width="100%" height="225">
             <div class="card-body">
               <p class="card-text">Denne her indeholder fakta og viden omkring Quick'n dirty video</p>
               <div class="d-flex justify-content-between align-items-center">
@@ -168,9 +177,10 @@
     <i class="fab fa-instagram footer-icon"></i>
     <i class="fas fa-envelope footer-icon"></i>
 
-  <p>© Copyright 2020 Jonas Hawthorn Mikkelsen</p>
+  <p class="copyright">© Copyright 2020 Jonas Hawthorn Mikkelsen</p>
 </div>
 </footer>
+<!-- JAVASCRIPT BRUGT TIL ALLE UNDERSIDER -->
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
       <script>window.jQuery || document.write('<script src="assets/js/vendor/jquery.slim.min.js"><\/script>')</script><script src="assets/dist/js/bootstrap.bundle.min.js"></script>
 </html>
