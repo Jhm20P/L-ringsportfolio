7ba202ed9bc111ef5f0557a8061858f0df60a82b
Jhm20P <71630698+Jhm20P@users.noreply.github.com>
2021-01-09 18:20:44 +0100

    Update hackathon.php

--- lærings-portfolio/hackathon.php
+++ lærings-portfolio/hackathon.php
@@ -1,12 +1,13 @@
 <!doctype html>
-<html lang="en">
+<html lang="da">
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
-    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="author" content="Jonas Hawthorn Mikkelsen">
     <meta name="generator" content="Jekyll v4.1.1">
-    <title>Carousel Template · Bootstrap</title>
+    <link rel="icon" href="favicon.ico">
+    <title>Hackathon</title>
 
     <link rel="canonical" href="https://getbootstrap.com/docs/4.5/examples/carousel/">
 
@@ -15,9 +16,12 @@
 
     <!-- Bootstrap core CSS -->
 <link href="assets/dist/css/bootstrap.min.css" rel="stylesheet">
+  <!-- EGEN CSS -->
 <link rel="stylesheet" href="underside.css">
 <link rel="stylesheet" href="bodyfix.css">
-
+<link rel="stylesheet" href="billede.css">
+<link rel="stylesheet" href="swot.css">
+<!-- INTERN CSS -->
     <style>
       .bd-placeholder-img {
         font-size: 1.125rem;
@@ -37,14 +41,15 @@
     <!-- Custom styles for this template -->
     <link href="carousel.css" rel="stylesheet">
   </head>
+  <!-- MENU BAR -->
   <?php include("Menu.inc"); ?>
 
 <main role="main">
-
+<!-- SEKTION MED OVERSKRIFT OG UNDEROVERSKRIFT -->
   <section class="jumbotron text-center">
     <div class="container">
       <h1>Hackathon</h1>
-      <p class="lead text-muted">Her kan man finde informationer omkring hvad jeg har lært angående design</p>
+      <p class="lead text-muted">Her kan man finde informationer omkring hvad vi som gruppe lave til Hackthon projektet</p>
     </div>
   </section>
 
@@ -58,41 +63,185 @@
     <!-- START THE FEATURETTES -->
 
     <hr class="featurette-divider">
-
+    <!-- FASE 1. EMPATHIZE -->
     <div class="row featurette">
       <div class="col-md-7">
-        <h2 class="featurette-heading">First featurette heading. <span class="text-muted">It’ll blow your mind.</span></h2>
-        <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+        <h2 class="featurette-heading">Skive Festival. <span class="sidenavn">Fase 1: Empathise.</span></h2>
+        <p class="lead">Hvad er Skive Festival?</p>
+        <ul>
+          <li>Skive Festival (tidligere ‘Skive Beach Party’) er en multi-festival som hvert år bliver holdt på strandtangen ved lystbådehavnen i Skive.</li>
+          <li>Skive Festival blev afholdt første gang i 1992.</li>
+          <li>Festivalen har 4 scener og byder på musik i alle genrer og teltplads til flere end 2000 gæster. Mad- og drikkeboder findes på pladsen. Parkering tæt på pladsen er muligt.</li>
+          <li>Skive Festival har en bred målgruppe, og vil gerne nå ud til flest mulige mennesker i alle aldre og fra hele landet. Det ses blandt andet ved, at der er indsat natbusser til Herning, Thisted og Viborg osv. Der er også mulighed for et mere luksuriøst “ophold”, med forplejning og ad libitum i det der bliver kaldt Waterfront.</li>
+          <li>Efter et dårligt år i 2010 og en dårlig periode efterfølgende, er Skive Festival ved at være “ovenpå” igen efter valg af ny bestyrelse i 2017.</li>
+          <li>Skive Festivals målsætning er, at få ca. 15.000 gæster i 2021.</li>
+          <li>En partoutbillet til 3 dage koster 995,00.</li>
+          <li>På Facebook har Skive Festival 29.208 likes og 28.246 følgere. På Instagram har de 2.320 følgere. På Facebook har 51 personer bedømt Skive Festival til 4,5 stjerner.</li>
+          <li>Kontantløs festival, med muligheder for at betale med en chip på armbåndet. Betalingskort og mobilepay kan også benyttes.</li>
+          <li>Skive Festivals sponsorer:</li>
+        </ul>
+        <p class="lead"><cite>-Naib, Christine, Alberte, Nuunu og Jonas, Moodle/<a class="google" href="https://docs.google.com/document/d/1ffEkHi55uFXJiCJxSv1xElptHP5b_gjfRpyyMFP7JOA/edit?usp=sharing" target="_blank">Google docs</a>, <time datetime="2020-09-04">4 September, 2020</time></cite></p>
       </div>
-      <div class="col-md-5">
-        <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 500x500"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"/><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg>
-      </div>
+      <img class="sponsor" src="fase1sponsor.png" width="450px" height="auto" alt="">
     </div>
-
+    <!-- FASE 1. EMPATHIZE SLUT -->
     <hr class="featurette-divider">
-
+    <!-- FASE 2. DEFINE -->
     <div class="row featurette">
       <div class="col-md-7 order-md-2">
-        <h2 class="featurette-heading">Oh yeah, it’s that good. <span class="text-muted">See for yourself.</span></h2>
-        <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+        <h2 class="featurette-heading">Skive Festival. <span class="sidenavn">Fase 2: Define.</span></h2>
+        <p class="lead">Problemdefinition:</p>
+        <ul>
+          <li>Hvordan kan Skive Festival nå ud til et større og ældre publikum?
+            <ul>
+              <li>Det er relevant, da Skive Festival har et ønske om en større indtjening og flere gæster på pladsen, samt at nå ud til det ældre publikum da de oftest har flere penge at bruge.</li>
+              <li>Vi kom frem til det, da vi brainstormede. Herefter talte vi om, at kun 1 i gruppen kendte til Skive Festival, selvom vi alle bor forholdsvis tæt.</li>
+            </ul>
+          </li>
+        </ul>
+        <p class="lead">SWOT analyse</p>
+        <p>Vi har valgt at lave en SWOT-analyse for at se på problematikker og muligheder på casen</p>
+        <p class="lead"><cite>-Naib, Christine, Alberte, Nuunu og Jonas, Moodle/<a class="google" href="https://docs.google.com/document/d/1tXgVGQh8aFL7Q_4SgU-TrO4Wx1Dw-jT75XSqjlSATG4/edit?usp=sharing" target="_blank">Google docs</a>, <time datetime="2020-09-04">4 September, 2020</time></cite></p>
       </div>
       <div class="col-md-5 order-md-1">
-        <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 500x500"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"/><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg>
+        <img class="swot-billede"src="swot.png" alt="">
       </div>
     </div>
+    <!-- Swot analyse -->
+    <table>
+      <div class="kontainer">
+        <!-- Styrker -->
+        <tr>
+        <td>
+        <div class="cicle">
+          <img src="s.png" alt="">
+        </div>
+        <div class="tekst">
+          <ul>
+            <li>Fået en god bestyrelse (2017)</li>
+            <li>Gode aftaler angående med udstyr og leverandør.</li>
+            <li>Billige billetpriser-Flere har råd til den</li>
+            <li>De har 4 scener - stort areal</li>
+          </ul>
+        </div>
+      </td>
+      <!-- svagheder -->
+      <td>
+      <div class="cicle2">
+        <img src="w.png" alt="">
+      </div>
+      <div class="tekst1">
+        <ul>
+          <li>Tradition</li>
+          <li>Unge 16-22 år.</li>
+          <li>Billige billetpriser-Flere har råd til den</li>
+          <li>Overskuddet fra festivalen går ud til de frivilliges foreninger</li>
+          <li>Billige billetpriser-Færre penge at gøre godt med.</li>
+          <li>Dårlig tidspunkt på året at holde det.</li>
+          <li>Ferierejser.</li>
+        </ul>
+      </div>
+    </td>
+    </tr>
 
+
+  </div>
+  <div class="kontainer">
+
+    <!-- muligheder -->
+    <tr>
+    <td>
+    <div class="cicle1">
+      <img src="o.png" alt="">
+    </div>
+    <div class="tekst">
+      <ul>
+        <li>Alle målgrupper i gallupkompasset</li>
+        <li>Lokalitet/location</li>
+        <li>Reklamer på sociale medier(konkurrencer, videoer, reach-out på facebook, instagram etc.)</li>
+        <li>Reklame Skilte/reklame plakater</li>
+        <li>Reklame biler.</li>
+        <li>Andre artister end lige musikere.</li>
+      </ul>
+    </div>
+  </td>
+
+  <!-- trusler -->
+  <td>
+  <div class="cicle3">
+    <img src="t.png" alt="">
+  </div>
+  <div class="tekst1">
+    <ul>
+      <li>Andre festivaler</li>
+      <li>Nærliggende byfester</li>
+      <li>Færre gæster</li>
+      <li>Forfester</li>
+      <li>Dårligt vejr</li>
+      <li>Konkurrence med musikere</li>
+      <li>Corona</li>
+      <li>Mister en leverandør</li>
+      <li>Myndigheder</li>
+    </ul>
+  </div>
+</td>
+</tr>
+</div>
+</table>
+  <!-- FASE 2. DEFINE SLUT -->
     <hr class="featurette-divider">
-
+    <!-- FASE 3. IDEATE -->
     <div class="row featurette">
       <div class="col-md-7">
-        <h2 class="featurette-heading">And lastly, this one. <span class="text-muted">Checkmate.</span></h2>
-        <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+        <h2 class="featurette-heading">Skive Festival. <span class="sidenavn">Fase 3: Ideate.</span></h2>
+        <p class="lead">Musikvideo.</p>
+        <ul>
+          <li>Indeholde en mascot (fisken Jaxon)</li>
+          <li>Den skal annonceres, når programmet offentliggøres og skal kunne bruges år efter år.</li>
+          <li>Baggrundsmusik(nostalgisk)</li>
+          <li>Annoncering af kunstnere(billeder)</li>
+          <li>Argumenter for hvorfor man skal vælge at tage Skive Festival</li>
+          <li>Strand/skov location filmes</li>
+          <li>Waterfront (billeder)</li>
+          <li>VIP området (billeder)</li>
+          <li>Pris billigt (billeder)</li>
+          <li>Camp</li>
+          <li>Hele pladsen (billeder)</li>
+          <li>Jingle</li>
+          <li>Videoen skal lægges på Facebook, der skal afholdes en konkurrence om x antal billetter. Dem skal man vinde ved at dele og skrive en kommentar.Månedskonkurrence.</li>
+        </ul>
+        <p class="lead"><cite>-Naib, Christine, Alberte, Nuunu og Jonas, Moodle/<a class="google" href="https://docs.google.com/document/d/1nmRdtTIkG7BEBAxZPvuhzublhj3Iz5dZjGYzKrUO9sE/edit?usp=sharing" target="_blank">Google docs</a>, <time datetime="2020-09-04">4 September, 2020</time></cite></p>
       </div>
       <div class="col-md-5">
-        <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 500x500"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"/><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg>
+        <img src="maskot.jpg" height="auto" width="450" alt="">
       </div>
     </div>
-
+    <!-- FASE 3. IDEATE SLUT -->
+    <hr class="featurette-divider">
+    <!-- FASE 4 -->
+    <div class="row featurette">
+      <div class="col-md-7 order-md-2">
+        <h2 class="featurette-heading">Hackathon. <span class="sidenavn">Fase 4.</span></h2>
+        <p class="lead">Test:</p>
+        <p style="text-align: center;">Så der fest på en strand</p>
+        <p style="text-align: center;">God musik og sang</p>
+        <p style="text-align: center;">Sol, blæst, sand og vand</p>
+        <p class="lead"></p>
+        <ul>
+          <li>For at optimere vores prototype, så skal maskotkostumet designes og indkøbes.</li>
+          <li>Videoen skal optages og redigeres.</li>
+          <li>Videoen skal fange seerens opmærksomhed på ca. 30 sekunder.</li>
+          <li>Jinglen skal være ørehængende og fangende.</li>
+          <li>De fleste af vores testpersoner kendte til landskendte maskotter og vidste hvor de ”stammer” fra. Den lokale maskot var der dog ingen, der kendte til.</li>
+          <li>Vores maskot skal være landskendt for at man husker ham og hvor han kommer fra</li>
+          <li>Jinglen skal ikke være for lang, da vores testpersoner blev præsenteret for nogle kendte jingles ikke kendte til den alt for lange jingle. ”Det kan ske en dag” fra lotto reklamen.</li>
+          <li>Videoen skal have det rigtige musikmix i baggrunden. Gerne nostalgisk musik.</li>
+          <li>Lyd, video og billeder skal passe sammen og være interessante.</li>
+        </ul>
+        <p class="lead"><cite>-Naib, Christine, Alberte, Nuunu og Jonas, Moodle/<a class="google" href="https://docs.google.com/document/d/1VT-46tz6_Jtiv3WtY36fgp5_MAkBIp767P6coyDbvy8/edit?usp=sharing" target="_blank">Google docs</a>, <time datetime="2020-09-04">4 September, 2020</time></cite></p>
+      </div>
+    </div>
+    <!-- FASE 4 SLUT -->
     <hr class="featurette-divider">
 
     <!-- /END THE FEATURETTES -->
@@ -108,10 +257,11 @@
       <i class="fab fa-instagram footer-icon"></i>
       <i class="fas fa-envelope footer-icon"></i>
 
-    <p>© Copyright 2020 Jonas Hawthorn Mikkelsen</p>
+    <p class="copyright">© Copyright 2020 Jonas Hawthorn Mikkelsen</p>
   </div>
   </footer>
 </main>
+<!-- JAVASCRIPT BRUGT TIL ALLE UNDERSIDER -->
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
       <script>window.jQuery || document.write('<script src="assets/js/vendor/jquery.slim.min.js"><\/script>')</script><script src="assets/dist/js/bootstrap.bundle.min.js"></script>
 </html>
