d3b8abbb9e7caf37d2fb1b65c3369a7d3e26c70e
Jhm20P <71630698+Jhm20P@users.noreply.github.com>
2021-01-09 01:13:50 +0100

    Update internettet.php

--- lærings-portfolio/internettet.php
+++ lærings-portfolio/internettet.php
@@ -1,12 +1,13 @@
 <!doctype html>
-<html lang="en">
+<html lang="da">
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
-    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="author" content="Jonas Hawthorn Mikkelsen">
     <meta name="generator" content="Jekyll v4.1.1">
-    <title>Carousel Template · Bootstrap</title>
+    <link rel="icon" href="favicon.ico">
+    <title>Internettets historie</title>
 
     <link rel="canonical" href="https://getbootstrap.com/docs/4.5/examples/carousel/">
 
@@ -15,9 +16,11 @@
 
     <!-- Bootstrap core CSS -->
 <link href="assets/dist/css/bootstrap.min.css" rel="stylesheet">
+  <!-- EGEN CSS -->
 <link rel="stylesheet" href="underside.css">
 <link rel="stylesheet" href="bodyfix.css">
-
+<link rel="stylesheet" href="pfix.css">
+  <!-- INTERN CSS -->
     <style>
       .bd-placeholder-img {
         font-size: 1.125rem;
@@ -37,14 +40,15 @@
     <!-- Custom styles for this template -->
     <link href="carousel.css" rel="stylesheet">
   </head>
+  <!-- MENU BAR -->
   <?php include("Menu.inc"); ?>
 
 <main role="main">
-
+<!-- SEKTION MED OVERSKRIFT OG UNDEROVERSKRIFT -->
   <section class="jumbotron text-center">
     <div class="container">
       <h1>Internettets historie</h1>
-      <p class="lead text-muted">Her kan man finde informationer omkring hvad jeg har lært angående design</p>
+      <p class="lead text-muted">Her kan man finde en beskrivelse samt svar på internettets historie</p>
     </div>
   </section>
 
@@ -58,41 +62,145 @@
     <!-- START THE FEATURETTES -->
 
     <hr class="featurette-divider">
-
+    <!-- HISTORY OF INTERNET -->
     <div class="row featurette">
       <div class="col-md-7">
-        <h2 class="featurette-heading">First featurette heading. <span class="text-muted">It’ll blow your mind.</span></h2>
-        <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
-      </div>
-      <div class="col-md-5">
-        <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 500x500"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"/><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg>
+        <h2 class="featurette-heading">History of the Internet.</h2>
+        <!-- LISTE SVAR -->
+        <ul>
+          <ul>
+            <p class="lead">Hvad er forskellen på begreberne "Internettet" og "World Wide Web"</p>
+            <li>
+              world wide web er det der behandlet af internettet. eksempel: Internettet er togene, world wide web er togene.
+            </li>
+            <li>
+              Tcp kontrol er en handshake agtig. UDP protocol er hvor der bare er information givet ud uden der er nogen for verificering(spilleregler)
+            </li>
+          </ul>
+          <ul>
+            <p class="lead punkt">Hvad kendetegner udviklingen af internettet?</p>
+            <li>
+              en måde hvor man nemmere og mere sikkert kunne transportere information rundt.
+            </li>
+          </ul>
+          <ul>
+            <p class="lead punkt">Hvorfra kommer begrebet "Inter-net" og hvad hentyder det til?</p>
+            <li>
+              Begrebet Inter-net kommer fra det franske netværk cyclades(siklad, tidlige 1970s)
+            </li>
+          </ul>
+          <ul>
+            <p class="lead punkt">Hvad er en "protokol"</p>
+            <li>
+              en/noget der verificere at informationen er blevet sendt.Procedure, spilleregler.
+            </li>
+          </ul>
+          <ul>
+            <p class="lead punkt">Hvornår startede udviklingen af internettet?</p>
+            <li>
+              1966
+            </li>
+            <li>
+              Ideen med internet er dele informations på kryds og tværs med hinanden.
+            </li>
+            <li>
+              Fysisk set kan internet have mange forskellige måder at kunne være på via frekvenser.
+            </li>
+          </ul>
+        </ul>
+        <!-- LISTE SVAR SLUT -->
+        <p class="lead"><cite>-Jonas, Moodle/<a class="google" href="https://docs.google.com/document/d/1FUQFvF1782Uk3g8gexx8JBx9QK1IzIK3TcHgslEt860/edit?usp=sharing" target="_blank">Google docs</a>, <time datetime="2020-09-11">11 sep, 2020</time></cite></p>
       </div>
     </div>
-
+    <!-- HISTORY OF INTERNET SLUT -->
     <hr class="featurette-divider">
-
+    <!-- NAVIGATING KNOWLEDGE -->
     <div class="row featurette">
       <div class="col-md-7 order-md-2">
-        <h2 class="featurette-heading">Oh yeah, it’s that good. <span class="text-muted">See for yourself.</span></h2>
-        <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
-      </div>
-      <div class="col-md-5 order-md-1">
-        <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 500x500"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"/><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg>
+        <h2 class="featurette-heading">Navigating knowledge.</h2>
+        <!-- LISTE SVAR -->
+        <ul>
+          <ul>
+            <p class="lead">Hvad er "information overload"</p>
+            <li>
+              Frustration, for meget information at tage ind på éngang, huske og bruge.
+            </li>
+          </ul>
+          <ul>
+            <p class="lead punkt">Hvad var ideen bag "World Palace of Knowledge"</p>
+            <li>
+              det at folk kunne dele viden. når folk ledte efter svar, så begyndte personalet at søge igennem arkivet for at finde svar.
+            </li>
+          </ul>
+          <ul>
+            <p class="lead punkt">Hvad var ideen bag "The Memex"?</p>
+            <li>
+              Forøge menneskets hukommelse/huskeevne. Hoved information(mest vigtige information) Process to ting på en gang.
+            </li>
+          </ul>
+          <ul>
+            <p class="lead punkt">Hvem var Douglas Engelbart og hvad er han kendt for?</p>
+            <li>
+              En radar tekniker. Mother of all demos(af hans team og ham på standford research institute i 1968(december)
+            </li>
+          </ul>
+          <ul>
+            <p class="lead punkt">Hvem opfandt begrebet "Hypertext" og hvad kunne det første Hypertext System.</p>
+            <li>
+              Ted Nelson.  wordprocession, multiple windows, hypertext links.
+            </li>
+            <li>
+              Hypertext er en samling af tekst der kan fører til en anden side. Hypertext dannes ved at der er flere dokumenter der forbindes.
+            </li>
+          </ul>
+          <ul>
+            <p class="lead punkt">Hvordan inspirerede Tim's far's artikel Tim?</p>
+            <li>
+              hans far læste en bog omkring hjerne research. Hans far ledte efter svar for hvordan computere ledte efter de samme opstillet links som hjernen gør.
+            </li>
+          </ul>
+          <ul>
+            <p class="lead punkt">Hvad var programmet Enquire</p>
+            <li>
+              Det sammenlignede dokumenter om personale, deres projekter de har lavet/var igang med, og den software de brugte.Programmet tjekkede efter “keywords”.
+            </li>
+          </ul>
+          <ul>
+            <p class="lead punkt">Hvad var problemet med ARPANET?</p>
+            <li>
+              brugt af mange forskellige computere, dette medførte at nogle computere havde problemer med at kommunikere.
+            </li>
+          </ul>
+          <ul>
+            <p class="lead punkt">Hvad var Tim's ide?</p>
+            <li>
+              Hyperlinks. Et standard sprog, der skulle gøre sådan at alle computere kunne kommunikere. De to grundlæggende brikker for det var HTML og URL
+            </li>
+          </ul>
+          <ul>
+            <p class="lead punkt">Hvad står HTML og URL for?</p>
+            <li>
+            HTML står for “Hypertext Markup Language” hvor URL står for “Universal Resource Locator”
+            </li>
+          </ul>
+          <ul>
+            <p class="lead punkt">Hvad er en Browser?</p>
+            <li>
+            En browser var det der skulle sikre sig den rigtige påvisning af et HTML dokument på ens computer. Browse/søge på nettet efter information og viden.
+            </li>
+          </ul>
+          <ul>
+            <p class="lead punkt">Hvem var Marc Andreasen?</p>
+            <li>
+            en student, der programmerede mosaic browser. Det var første gang man kunne indsætte billeder og hyperlinks der kunne bliver aktiveret bare ved et enkelt museklik.
+            </li>
+          </ul>
+        </ul>
+        <!-- LISTE SVAR SLUT -->
+        <p class="lead"><cite>-Jonas, Moodle/<a class="google" href="https://docs.google.com/document/d/1FUQFvF1782Uk3g8gexx8JBx9QK1IzIK3TcHgslEt860/edit?usp=sharing" target="_blank">Google docs</a>, <time datetime="2020-09-11">11 sep, 2020</time></cite></p>
       </div>
     </div>
-
-    <hr class="featurette-divider">
-
-    <div class="row featurette">
-      <div class="col-md-7">
-        <h2 class="featurette-heading">And lastly, this one. <span class="text-muted">Checkmate.</span></h2>
-        <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
-      </div>
-      <div class="col-md-5">
-        <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 500x500"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"/><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg>
-      </div>
-    </div>
-
+    <!-- NAVIGATING KNOWLEDGE SLUT -->
     <hr class="featurette-divider">
 
     <!-- /END THE FEATURETTES -->
@@ -108,10 +216,11 @@
       <i class="fab fa-instagram footer-icon"></i>
       <i class="fas fa-envelope footer-icon"></i>
 
-    <p>© Copyright 2020 Jonas Hawthorn Mikkelsen</p>
+    <p class="copyright">© Copyright 2020 Jonas Hawthorn Mikkelsen</p>
   </div>
   </footer>
 </main>
+<!-- JAVASCRIPT BRUGT TIL ALLE UNDERSIDER -->
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
       <script>window.jQuery || document.write('<script src="assets/js/vendor/jquery.slim.min.js"><\/script>')</script><script src="assets/dist/js/bootstrap.bundle.min.js"></script>
 </html>
