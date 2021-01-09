b9dd4027f2092f29438dc7d73c218f34302a9f04
Jhm20P <71630698+Jhm20P@users.noreply.github.com>
2021-01-09 20:02:41 +0100

    Update projekter.php

--- lærings-portfolio/projekter.php
+++ lærings-portfolio/projekter.php
@@ -1,12 +1,13 @@
 <!doctype html>
-<html lang="en">
+<html lang="da">
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
-    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="author" content="Jonas Hawthorn Mikkelsen">
     <meta name="generator" content="Jekyll v4.1.1">
-    <title>Album example · Bootstrap</title>
+    <link rel="icon" href="favicon.ico">
+    <title>Projekter</title>
 
     <!-- Javascript for iconer fra fontawesome -->
     <script src="https://kit.fontawesome.com/c43d39253a.js" crossorigin="anonymous"></script>
@@ -15,8 +16,9 @@
 
     <!-- Bootstrap core CSS -->
 <link href="assets/dist/css/bootstrap.min.css" rel="stylesheet">
+<!-- EGEN CSS -->
 <link rel="stylesheet" href="underside.css">
-
+<!-- INTERN CSS -->
     <style>
       .bd-placeholder-img {
         font-size: 1.125rem;
@@ -34,14 +36,15 @@
       }
     </style>
     <!-- Custom styles for this template -->
+    <!-- MENU BAR -->
     <?php include("Menu.inc"); ?>
 
 <main role="main">
-
+<!-- SEKTION MED OVERSKRIFT OG UNDEROVERSKRIFT -->
   <section class="jumbotron text-center">
     <div class="container">
-      <h1>Projekter</h1>
-      <p class="lead text-muted">Something short and leading about the collection below—its contents, the creator, etc. Make it short and sweet, but not too short so folks don’t simply skip over it entirely.</p>
+      <h1 class="text-muted">Projekter</h1>
+      <p class="lead text-muted">Her vil man kunne se en oversigt over de projekter jeg har været igennem</p>
     </div>
   </section>
 
@@ -49,9 +52,10 @@
     <div class="container">
 
       <div class="row">
+        <!-- HACKATHON -->
         <div class="col-md-4">
           <div class="card mb-4 shadow-sm">
-            <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+            <img class="card-img-top" src="hackaton.png" width="100%" height="225">
             <div class="card-body">
               <p class="card-text">Her vil jeg komme til at gå over hvad Hackathon er og hvilket emne min   gruppe og jeg fokuserede på</p>
               <div class="d-flex justify-content-between align-items-center">
@@ -62,9 +66,10 @@
             </div>
           </div>
         </div>
+        <!-- DESIGN THINKING -->
         <div class="col-md-4">
           <div class="card mb-4 shadow-sm">
-            <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+            <img class="card-img-top" src="designt.png" width="100%" height="225">
             <div class="card-body">
               <p class="card-text">Her vil jeg komme inde hvad Design thinking er, hvilken process min gruppe og jeg tog i dette projekt.</p>
               <div class="d-flex justify-content-between align-items-center">
@@ -75,9 +80,10 @@
             </div>
           </div>
         </div>
+        <!-- ONEPAGE -->
         <div class="col-md-4">
           <div class="card mb-4 shadow-sm">
-            <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+          <img class="card-img-top" src="onepage.png" width="100%" height="225">
             <div class="card-body">
               <p class="card-text">Her vil jeg komme ind på hvad vores onepage projekt gik ud på, og hvad vi fik lavet.</p>
               <div class="d-flex justify-content-between align-items-center">
@@ -88,10 +94,10 @@
             </div>
           </div>
         </div>
-
+        <!-- LÆRINGSPORTFOLIO -->
         <div class="col-md-4">
           <div class="card mb-4 shadow-sm">
-            <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+            <img class="card-img-top" src="portfolio.png" width="100%" height="225">
             <div class="card-body">
               <p class="card-text">Her vil jeg komme til gå ind på hvordan jeg har lavet min  Læringsportfolio og hvad mine tanker har været under processen.</p>
               <div class="d-flex justify-content-between align-items-center">
@@ -115,9 +121,10 @@
     <i class="fab fa-instagram footer-icon"></i>
     <i class="fas fa-envelope footer-icon"></i>
 
-  <p>© Copyright 2020 Jonas Hawthorn Mikkelsen</p>
+  <p class="copyright">© Copyright 2020 Jonas Hawthorn Mikkelsen</p>
 </div>
 </footer>
+<!-- JAVASCRIPT BRUGT TIL ALLE UNDERSIDER -->
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
       <script>window.jQuery || document.write('<script src="assets/js/vendor/jquery.slim.min.js"><\/script>')</script><script src="assets/dist/js/bootstrap.bundle.min.js"></script>
 </html>
