e9c247d0db867e4d72d310f6443b90fc11cec654
Jhm20P <71630698+Jhm20P@users.noreply.github.com>
2021-01-09 16:36:37 +0100

    Update paradigmer.php

--- lærings-portfolio/paradigmer.php
+++ lærings-portfolio/paradigmer.php
@@ -1,12 +1,13 @@
 <!doctype html>
-<html lang="en">
+<html lang="da">
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
-    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="author" content="Jonas Hawthorn Mikkelsen">
     <meta name="generator" content="Jekyll v4.1.1">
-    <title>Carousel Template · Bootstrap</title>
+    <link rel="icon" href="favicon.ico">
+    <title>Paradigmer</title>
 
     <link rel="canonical" href="https://getbootstrap.com/docs/4.5/examples/carousel/">
 
@@ -15,9 +16,11 @@
 
     <!-- Bootstrap core CSS -->
 <link href="assets/dist/css/bootstrap.min.css" rel="stylesheet">
+  <!-- EGEN CSS -->
 <link rel="stylesheet" href="underside.css">
 <link rel="stylesheet" href="bodyfix.css">
-
+<link rel="stylesheet" href="billede.css">
+  <!-- INTERN CSS -->
     <style>
       .bd-placeholder-img {
         font-size: 1.125rem;
@@ -37,14 +40,15 @@
     <!-- Custom styles for this template -->
     <link href="carousel.css" rel="stylesheet">
   </head>
+    <!-- MENU BAR -->
   <?php include("Menu.inc"); ?>
 
 <main role="main">
-
+<!-- SEKTION MED OVERSKRIFT OG UNDEROVERSKRIFT -->
   <section class="jumbotron text-center">
     <div class="container">
       <h1>Paradigmer</h1>
-      <p class="lead text-muted">Her kan man finde informationer omkring hvad jeg har lært angående design</p>
+      <p class="lead text-muted">Her kan man finde informationer omkring hvad jeg har lært angående paradigmer</p>
     </div>
   </section>
 
@@ -58,41 +62,125 @@
     <!-- START THE FEATURETTES -->
 
     <hr class="featurette-divider">
-
+    <!-- HUANISTISKE PARADIGME -->
     <div class="row featurette">
       <div class="col-md-7">
-        <h2 class="featurette-heading">First featurette heading. <span class="text-muted">It’ll blow your mind.</span></h2>
-        <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
-      </div>
-      <div class="col-md-5">
-        <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 500x500"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"/><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg>
+        <h2 class="featurette-heading">Humanistiske paradigme.</h2>
+        <p class="lead">I det humanistiske paradigme bliver der fokuseret på individet selv som det primære mål. I kommunikationsforskningen ligger fokusset tildeles på modtageren, der også bliver set som aktiv samt medskabende. Det humanistiske paradigme består af fagområderne: sprog, kultur, psykologi, læring, organisation, antropologi, kunst, informationsteknologi samt flere fagområder. De retninger som er mest dominerende inden for dette paradigme er, hermeneutikken, fænomenologien og til sidst den symbolske interaktionisme. I dansk medieforskning inden for det humanistiske paradigme, siger de at al menneskelig interaktion/kontakt med hinanden er bygget på kommunikationen. Når man skal lave undersøgelser inden for det humanistiske paradigme vil man bruge kvalitative undersøgelse, som f.eks. et interview. Grunden til at man bruger den her form for undersøgelse, er at man gerne ind på hvordan samt hvad modtageren oplever og på den måde kan skabe en dybere forståelse inden for det.  Det humanistiske paradigme er også kendt som interaktionsparadigmet, da det er i grund og bund opbygget omkring interaktion mellem mennesker. </p>
+        <p class="lead">Typiske træk ved det humanistiske paradigme</p>
+        <ul>
+          <li>Fortolkning</li>
+          <li>Kvalitativ tilgang</li>
+          <li>Modtagerorienteret</li>
+          <li>Subjektivitet</li>
+          <li>Mennesket ses som aktivt</li>
+          <li>Induktion</li>
+          <li>Interaktion mellem mennesker</li>
+          <li>Situationsbestemt</li>
+        </ul>
+        <p class="lead"><cite>-Kommunikation i multimediedesign, Side 67-69, <time datetime="2021-01-5">5 Januar 2021</time></cite></p>
       </div>
     </div>
-
+    <!-- HUANISTISKE PARADIGME SLUT -->
     <hr class="featurette-divider">
-
+    <!-- IMK-MODELLEN -->
+    <div class="row featurette">
+      <div class="col-md-7">
+        <h2 class="featurette-heading">Humanistiske paradigme. <span class="sidenavn">IMK-modellen</span></h2>
+        <p class="lead">IMK-modellen er en klassisk kommunikationsmodel her i danmark angående kommunikationsfag. Betegnelsen IMK står for International Markeds Kommunikation. </p>
+        <p class="lead">Modellens elementer</p>
+        <ul>
+          <li>Afsender
+            <ul>
+              <li>Er den person som har sørget for kommunikationen samt den der har besluttet hvordan og hvornår det skal finde sted. Den teksteksterne afsender kan være en reklameannonce, Det vil sige at i dette tilfælde så er det reklamebureauet der har sat det op. Den tekstinterne afsender er vedkommende der explicit markeret i teksten. Den primære afsender vil være en virksomhed f.eks.  som er synlig i en annonce, hvor den sekundære afsender kunne f.eks. være en form for specialist inden for det produkt der er i annoncen.
+              </li>
+            </ul>
+          </li>
+          <li>Modtager
+            <ul>
+              <li>En modtager er den person som tager imod den kommunikation/budskab som afsenderen kommer med. Ligesom ved afsender, så skelner man også mellem tekstekstern og tekstintern. Den tekstinterne modtager vil være markeret i teksten fra afsenderen, hvor den teksteksterne ikke er tilstede i teksten selv, men derimod vedkommende der læser teksten, hvor det stadig træder i kraft selvom at modtageren er uden for målgruppen.</li>
+            </ul>
+          </li>
+          <li>Teksten
+            <ul>
+              <li>Teksten er den del der skal forstås som den kommunikative sammenhæng mellem afsender og modtager i forhold til afsenderens produktion og modtagerens reception</li>
+            </ul>
+          </li>
+          <li>Referenten
+            <ul>
+              <li>Referenten er alt det som der bliver refereret til i teksten. Det kan f.eks. være et produkt, dog vil det typisk være den effekt, som et produkt giver, eller det kan også være et brand som står bag et produkt.</li>
+            </ul>
+          </li>
+          <li>Koden
+            <ul>
+              <li>Koden er det der kan forstås som det sprog modtageren forstår. Det vil også betyde at ens valg af medier samt gerne vil have en indflydelse på den kode. Man skal selvfølgelig huske at nogle ting betyder noget forskelligt afhængig af kultur</li>
+            </ul>
+          </li>
+          <li>Kontekst/kultur
+            <ul>
+              <li>Er den konkrete situation, som indebærer kommunikationssituationen. Kan beskrives med andre ord som, “Hvem”, “Hvad”, “Hvor” og “Hvornår”. Men der vil så stadig den kultur, som påvirker situationen.  Kultur er noget der kan ses på flere forskellige niveauer, som er lige fra det nære til det nationale samt internationale.Din kontekst samt den kultur du arbejder i, vil være det der afgøre hvilket medie og retoriske strategier/metoder du vælger at bruge. Man skal også have det i bagtanken, at det der er positivt i en kultur kan være negativ i en anden.</li>
+            </ul>
+          </li>
+          <li>Medier
+            <ul>
+              <li>Medie er der hvor kommunikationssituationen befinder sig.  Et medie kan være printede medier som aviser, elektroniske medier kan være Facebook eller Twitter eller det kan også være mennesket selv der fungere som et medie. Det er vigtigt at du gøre dig nogle overvejelser omkring anvendelse samt situation, da medier har flere forskellige forhold af indholdsmæssig samt praktisk karakter.</li>
+            </ul>
+          </li>
+          <li>Genre
+            <ul>
+              <li>I IMK-modellen kan der ses et budskab for hvordan kultur/kontekst og medier samt retoriske strategier kan arbejde sammen.
+                <ul>
+                  <li>Trækstrukturer forstås som de sproglige træk som der hver især er med til at opfylde formålet for det kommunikative. Det vil så også sige at tekster som har det samme formål, typisk også har nogle træk tilfælles.</li>
+                  <li>Retoriske virkemidler kan være anvendelse af fagbegreber, slogans, indforstået ordspil eller intertekstuelle referencer samt logo og farver.</li>
+                </ul>
+              </li>
+            </ul>
+          </li>
+        </ul>
+        <p class="lead"><cite>-Kommunikation i multimediedesign, Side 69-72, <time datetime="2021-01-5">5 Januar 2021</time></cite></p>
+      </div>
+      <div class="col-md-5">
+        <img src="imkmodellen.png" alt="">
+      </div>
+    </div>
+    <!-- IMK-MODELLEN SLUT -->
+    <hr class="featurette-divider">
+    <!-- SAMFUNDSVIDENSKABELIGE PARADIGME -->
     <div class="row featurette">
       <div class="col-md-7 order-md-2">
-        <h2 class="featurette-heading">Oh yeah, it’s that good. <span class="text-muted">See for yourself.</span></h2>
-        <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+        <h2 class="featurette-heading">Samfundsvidenskabelige paradigme.</h2>
+        <p class="lead">Det samfundsvidenskabelige paradigme har fokus på afsenderen og dens placering i omverdenen. Det er i dette paradigme at afsenderen ses som den styrende i forhold til kommunikationsprocessen, hvor paradigmet er styres tildeles af en forståelse for kommunikation som transmission. Hvis man kigger på den traditionelle del ved samfundsvidenskabelige paradigme, vil man kigge på fagområderne samfundsvidenskab, sociologi, politik, økonomi, jura osv. De klassiske retninger inden for det videnskabsteoretiske i dette paradigme, vil man kigge på systemteori samt funktionalisme, der egentligt oprindeligt stammer fra en biologisk forklaring sammenhæng. Det skal dog siges at inden for de seneste år, har det samfundsvidenskabelige paradigme taget flere metoder samt teori fra det humanistiske paradigme. Når man skal undersøge noget i det samfundsvidenskabelige paradigme, vil man typisk bruge kvantitative metode for at få en mere generel forståelse og data på det man undersøger.</p>
+        <p class="lead">De typiske træk ved det samfundsvidenskabelige paradigme.</p>
+        <ul>
+          <li>Modtageren ses som passiv</li>
+          <li>Afsenderorienteret</li>
+          <li>Objektivitet</li>
+          <li>Kvantitativ tilgang/undersøgelse</li>
+          <li>Stabile omgivelser</li>
+          <li>Handlingsanvisende</li>
+          <li>Deduktion</li>
+          <li>Transmission</li>
+          <li>Forklaring</li>
+        </ul>
+        <p class="lead"><cite>-Kommunikation i multimediedesign, Side 76-77, <time datetime="2021-01-5">5 Januar 2021</time></cite></p>
+      </div>
+    </div>
+    <!-- SAMFUNDSVIDENSKABELIGE PARADIGME SLUT -->
+    <hr class="featurette-divider">
+    <!-- S-R OG S-O-R MODELLEN -->
+    <div class="row featurette">
+      <div class="col-md-7 order-md-2">
+        <h2 class="featurette-heading">S-R-modellen.</h2>
+        <p class="lead">Kanyleteorien og S-R-modellen minder meget om hinanden da begge modeller handler om at mennesket styres på baggrund af påvirkning eller stimulering.  “S” står for stimulus og “R” står for respons, hvilket henviser til at efter man har kommet ud med sit budskab, vil der følge en respons. Jo kraftigere ens budskab, jo en større respons vil du få tilbage.</p>
+        <h2 class="featurette-heading">S-O-R-modellen</h2>
+        <p class="lead">I S-O-R-modellen bliver kommunikationen gennemført gennem en form for organisme, hvor det ikke sker direkte som det gør i S-R-modellen. For hvordan det med “organisme” forstås er alt efter modellens anvendelighed.</p>
+        <p class="lead"><cite>-Kommunikation i multimediedesign, Side 78-79, <time datetime="2021-01-5">5 Januar 2021</time></cite></p>
       </div>
       <div class="col-md-5 order-md-1">
-        <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 500x500"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"/><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg>
+        <img src="sor.png" height="auto" width="450" alt="">
       </div>
     </div>
-
-    <hr class="featurette-divider">
-
-    <div class="row featurette">
-      <div class="col-md-7">
-        <h2 class="featurette-heading">And lastly, this one. <span class="text-muted">Checkmate.</span></h2>
-        <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
-      </div>
-      <div class="col-md-5">
-        <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 500x500"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"/><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg>
-      </div>
-    </div>
-
+    <!-- S-R OG S-O-R MODELLEN SLUT -->
     <hr class="featurette-divider">
 
     <!-- /END THE FEATURETTES -->
@@ -108,10 +196,11 @@
       <i class="fab fa-instagram footer-icon"></i>
       <i class="fas fa-envelope footer-icon"></i>
 
-    <p>© Copyright 2020 Jonas Hawthorn Mikkelsen</p>
+    <p class="copyright">© Copyright 2020 Jonas Hawthorn Mikkelsen</p>
   </div>
   </footer>
 </main>
+<!-- JAVASCRIPT BRUGT TIL ALLE UNDERSIDER -->
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
       <script>window.jQuery || document.write('<script src="assets/js/vendor/jquery.slim.min.js"><\/script>')</script><script src="assets/dist/js/bootstrap.bundle.min.js"></script>
 </html>
