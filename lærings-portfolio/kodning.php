b27cb2b21586f543045b72513c26da5d8d533741
Jhm20P <71630698+Jhm20P@users.noreply.github.com>
2021-01-09 19:52:24 +0100

    Update kodning.php

--- lærings-portfolio/kodning.php
+++ lærings-portfolio/kodning.php
@@ -1,12 +1,13 @@
 <!doctype html>
-<html lang="en">
+<html lang="da">
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
-    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="author" content="Jonas Hawthorn Mikkelsen">
     <meta name="generator" content="Jekyll v4.1.1">
-    <title>Album example · Bootstrap</title>
+    <link rel="icon" href="favicon.ico">
+    <title>Kodning</title>
 
     <!-- Javascript for iconer fra fontawesome -->
     <script src="https://kit.fontawesome.com/c43d39253a.js" crossorigin="anonymous"></script>
@@ -15,8 +16,9 @@
 
     <!-- Bootstrap core CSS -->
 <link href="assets/dist/css/bootstrap.min.css" rel="stylesheet">
+  <!-- EGEN CSS -->
 <link rel="stylesheet" href="underside.css">
-
+<!-- INTERN CSS -->
     <style>
       .bd-placeholder-img {
         font-size: 1.125rem;
@@ -34,145 +36,140 @@
       }
     </style>
     <!-- Custom styles for this template -->
+    <!-- MENU BAR -->
     <?php include("Menu.inc"); ?>
 
-<main role="main">
+    <main role="main">
+<!-- SEKTION MED OVERSKRIFT OG UNDEROVERSKRIFT -->
+      <section class="jumbotron text-center">
+        <div class="container">
+          <h1 class="text-muted">Kodning</h1>
+          <p class="lead text-muted">Her kan man finde informationer omkring hvad jeg har lært angående Kodning af Hjemmesider</p>
+        </div>
+      </section>
 
-  <section class="jumbotron text-center">
-    <div class="container">
-      <h1>Kodning</h1>
-      <p class="lead text-muted">Her kan man finde informationer omkring hvad jeg har lært angående Kodning af Hjemmesider</p>
-    </div>
-  </section>
+      <div class="album py-5 bg-light">
+        <div class="container">
 
-  <div class="album py-5 bg-light">
-    <div class="container">
-
-      <div class="row">
-        <div class="col-md-4">
-          <div class="card mb-4 shadow-sm">
-            <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-            <div class="card-body">
-              <p class="card-text">Indeholder fakta og viden omkring HTML og dens syntax</p>
-              <div class="d-flex justify-content-between align-items-center">
-                <div class="btn-group">
-                  <a href="htmlsyntax.php"><button type="button" class="btn btn-sm btn-outline-secondary">Se mere</button></a>
+          <div class="row">
+            <!-- HTML SYNTAX -->
+            <div class="col-md-4">
+              <div class="card mb-4 shadow-sm">
+                <img class="card-img-top" src="htmlsyntax.png" width="100%" height="225">
+                <div class="card-body">
+                  <p class="card-text">Indeholder fakta og viden omkring HTML og dens syntax</p>
+                  <div class="d-flex justify-content-between align-items-center">
+                    <div class="btn-group">
+                    <a href="htmlsyntax.php"><button type="button" class="btn btn-sm btn-outline-secondary">Se mere</button></a>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        </div>
-        <div class="col-md-4">
-          <div class="card mb-4 shadow-sm">
-            <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-            <div class="card-body">
-              <p class="card-text">Indeholder fakta og viden omkring CSS og dens syntax</p>
-              <div class="d-flex justify-content-between align-items-center">
-                <div class="btn-group">
-                  <a href="csssyntax.php"><button type="button" class="btn btn-sm btn-outline-secondary">Se mere</button></a>
+            <!-- CSS SYNTAX -->
+            <div class="col-md-4">
+              <div class="card mb-4 shadow-sm">
+              <img class="card-img-top" src="csssyntax.png" width="100%" height="225">
+                <div class="card-body">
+                  <p class="card-text">Indeholder fakta og viden omkring CSS og dens syntax</p>
+                  <div class="d-flex justify-content-between align-items-center">
+                    <div class="btn-group">
+                    <a href="csssyntax.php"><button type="button" class="btn btn-sm btn-outline-secondary">Se mere</button></a>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        </div>
-        <div class="col-md-4">
-          <div class="card mb-4 shadow-sm">
-            <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-            <div class="card-body">
-              <p class="card-text">Her kan  du se mere omkring hvad bootstrap er og hvad det kan bruges til</p>
-              <div class="d-flex justify-content-between align-items-center">
-                <div class="btn-group">
-                  <a href="bootstrap.php"><button type="button" class="btn btn-sm btn-outline-secondary">Se mere</button></a>
+            <!-- BOOTSTRAP -->
+            <div class="col-md-4">
+              <div class="card mb-4 shadow-sm">
+                <img class="card-img-top" src="bootstrap.png" width="100%" height="225">
+                <div class="card-body">
+                  <p class="card-text">Her kan  du se mere omkring hvad bootstrap er og hvad det kan bruges til</p>
+                  <div class="d-flex justify-content-between align-items-center">
+                    <div class="btn-group">
+                    <a href="bootstrap.php"><button type="button" class="btn btn-sm btn-outline-secondary">Se mere</button></a>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        </div>
-
-        <div class="col-md-4">
-          <div class="card mb-4 shadow-sm">
-            <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-            <div class="card-body">
-              <p class="card-text">Dette indeholder information omkring Internettet og dens historie.</p>
-              <div class="d-flex justify-content-between align-items-center">
-                <div class="btn-group">
-                  <a href="internettet.php"><button type="button" class="btn btn-sm btn-outline-secondary">Se mere</button></a>
+            <!-- INTERNETTETS HISTORIE -->
+            <div class="col-md-4">
+              <div class="card mb-4 shadow-sm">
+                <img class="card-img-top" src="Internettet.png" width="100%" height="225">
+                <div class="card-body">
+                  <p class="card-text">Dette indeholder information omkring Internettet og dens historie.</p>
+                  <div class="d-flex justify-content-between align-items-center">
+                    <div class="btn-group">
+                      <a href="Internettet.php"><button type="button" class="btn btn-sm btn-outline-secondary">Se mere</button></a>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        </div>
-        <div class="col-md-4">
-          <div class="card mb-4 shadow-sm">
-            <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-            <div class="card-body">
-              <p class="card-text">Dette indeholder viden omkring hvad responsiv design er.</p>
-              <div class="d-flex justify-content-between align-items-center">
-                <div class="btn-group">
-                  <a href="responsiv.php"><button type="button" class="btn btn-sm btn-outline-secondary">Se mere</button></a>
+            <!-- RESPONSIV DESIGN -->
+            <div class="col-md-4">
+              <div class="card mb-4 shadow-sm">
+                <img class="card-img-top" src="respdesign.png" width="100%" height="225">
+                <div class="card-body">
+                  <p class="card-text">Dette indeholder viden omkring hvad responsiv design er.</p>
+                  <div class="d-flex justify-content-between align-items-center">
+                    <div class="btn-group">
+                    <a href="responsiv.php"><button type="button" class="btn btn-sm btn-outline-secondary">Se mere</button></a>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        </div>
-        <div class="col-md-4">
-          <div class="card mb-4 shadow-sm">
-            <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-            <div class="card-body">
-              <p class="card-text">Dette indeholder viden omkring Versionsstyring ved brug af Github</p>
-              <div class="d-flex justify-content-between align-items-center">
-                <div class="btn-group">
-                  <a href="versionsstyring.php"><button type="button" class="btn btn-sm btn-outline-secondary">Se mere</button></a>
+            <!-- VERSIONSSTYRING -->
+            <div class="col-md-4">
+              <div class="card mb-4 shadow-sm">
+                <img class="card-img-top" src="version.png" width="100%" height="225">
+                <div class="card-body">
+                  <p class="card-text">Dette indeholder viden omkring Versionsstyring ved brug af Github</p>
+                  <div class="d-flex justify-content-between align-items-center">
+                    <div class="btn-group">
+                      <a href="versionsstyring.php"><button type="button" class="btn btn-sm btn-outline-secondary">Se mere</button></a>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        </div>
-
-        <div class="col-md-4">
-          <div class="card mb-4 shadow-sm">
-            <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-            <div class="card-body">
-              <p class="card-text">Her kommer jeg ind på HTML og CSS workshoppen, hvilket emner der er fokuseret på her.</p>
-              <div class="d-flex justify-content-between align-items-center">
-                <div class="btn-group">
-                  <a href="htmlcsswork.php"><button type="button" class="btn btn-sm btn-outline-secondary">Se mere</button></a>
+            <!-- HTML OG CSS WORKSHOP -->
+            <div class="col-md-4">
+              <div class="card mb-4 shadow-sm">
+                <img class="card-img-top" src="workshop.png" width="100%" height="225">
+                <div class="card-body">
+                  <p class="card-text">Her kommer jeg ind på HTML og CSS workshoppen, hvilket emner der er fokuseret på her.</p>
+                  <div class="d-flex justify-content-between align-items-center">
+                    <div class="btn-group">
+                      <a href="htmlcsswork.php"><button type="button" class="btn btn-sm btn-outline-secondary">Se mere</button></a>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        </div>
-        <div class="col-md-4">
-          <div class="card mb-4 shadow-sm">
-            <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-            <div class="card-body">
-              <p class="card-text">Her kommer jeg ind på hvad PHP er, og hvad det kan bruges til.</p>
-              <div class="d-flex justify-content-between align-items-center">
-                <div class="btn-group">
-                  <a href="php.php"><button type="button" class="btn btn-sm btn-outline-secondary">Se mere</button></a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="col-md-4">
-          <div class="card mb-4 shadow-sm">
-            <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-            <div class="card-body">
-              <p class="card-text">Her kommer jeg ind på hvad serverside er.</p>
-              <div class="d-flex justify-content-between align-items-center">
-                <div class="btn-group">
-                  <a href="serverside.php"><button type="button" class="btn btn-sm btn-outline-secondary">Se mere</button></a>
+            <!-- PHP -->
+            <div class="col-md-4">
+              <div class="card mb-4 shadow-sm">
+                <img class="card-img-top" src="php.png" width="100%" height="225">
+                <div class="card-body">
+                  <p class="card-text">Her kommer jeg ind på hvad PHP er, og hvad det kan bruges til.</p>
+                  <div class="d-flex justify-content-between align-items-center">
+                    <div class="btn-group">
+                      <a href="php.php"><button type="button" class="btn btn-sm btn-outline-secondary">Se mere</button></a>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  </div>
 
-</main>
+
+    </main>
 
 <footer class="white-section" id="footer">
 <div class="container-fluid">
@@ -181,9 +178,10 @@
     <i class="fab fa-instagram footer-icon"></i>
     <i class="fas fa-envelope footer-icon"></i>
 
-  <p>© Copyright 2020 Jonas Hawthorn Mikkelsen</p>
+  <p class="copyright">© Copyright 2020 Jonas Hawthorn Mikkelsen</p>
 </div>
 </footer>
+<!-- JAVASCRIPT BRUGT TIL ALLE UNDERSIDER -->
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
       <script>window.jQuery || document.write('<script src="assets/js/vendor/jquery.slim.min.js"><\/script>')</script><script src="assets/dist/js/bootstrap.bundle.min.js"></script>
 </html>
