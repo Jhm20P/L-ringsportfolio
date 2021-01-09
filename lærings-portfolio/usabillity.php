575e21e8dccfaf596c60e6b71a75776a062c262f
Jhm20P <71630698+Jhm20P@users.noreply.github.com>
2021-01-09 14:28:39 +0100

    Update usabillity.php

--- lærings-portfolio/usabillity.php
+++ lærings-portfolio/usabillity.php
@@ -1,12 +1,13 @@
 <!doctype html>
-<html lang="en">
+<html lang="da">
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
-    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="author" content="Jonas Hawthorn Mikkelsen">
     <meta name="generator" content="Jekyll v4.1.1">
-    <title>Carousel Template · Bootstrap</title>
+    <link rel="icon" href="favicon.ico">
+    <title>Usabillity test</title>
 
     <link rel="canonical" href="https://getbootstrap.com/docs/4.5/examples/carousel/">
 
@@ -15,9 +16,10 @@
 
     <!-- Bootstrap core CSS -->
 <link href="assets/dist/css/bootstrap.min.css" rel="stylesheet">
+  <!-- EGEN CSS -->
 <link rel="stylesheet" href="underside.css">
 <link rel="stylesheet" href="bodyfix.css">
-
+  <!-- INTERN CSS -->
     <style>
       .bd-placeholder-img {
         font-size: 1.125rem;
@@ -37,14 +39,15 @@
     <!-- Custom styles for this template -->
     <link href="carousel.css" rel="stylesheet">
   </head>
+  <!-- MENU BAR -->
   <?php include("Menu.inc"); ?>
 
 <main role="main">
-
+<!-- SEKTION MED OVERSKRIFT OG UNDEROVERSKRIFT -->
   <section class="jumbotron text-center">
     <div class="container">
       <h1>Usabillity test</h1>
-      <p class="lead text-muted">Her kan man finde informationer omkring hvad jeg har lært angående design</p>
+      <p class="lead text-muted">Her kan man finde informationer omkring hvad jeg har lært omkring Usability og de forskellige test.</p>
     </div>
   </section>
 
@@ -58,41 +61,64 @@
     <!-- START THE FEATURETTES -->
 
     <hr class="featurette-divider">
-
+    <!-- Metode valg -->
     <div class="row featurette">
       <div class="col-md-7">
-        <h2 class="featurette-heading">First featurette heading. <span class="text-muted">It’ll blow your mind.</span></h2>
-        <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+        <h2 class="featurette-heading">Metode valg.</h2>
+        <p class="lead">Vi har under vores undersøgelser valgt nogle forskellige undersøgelses former, af dem som vi har læst om i bogen, Usability. Vi endte derfor ud med at benytte de nævnte test som er beskrevet nedenfor, fordi ud fra hvad vi har læst, virkede det til at være de bedste metoder til vores undersøgelser.</p>
+        <p class="lead"><cite>-Andreas, Veli og Jonas, Moodle/<a class="google" href="https://itcn-my.sharepoint.com/:w:/g/personal/10741_student_eadania_dk/EZBUH-M0lBdPt6lZy7cqm6QBg4g2nbzlip7QKr4vXX69Uw?e=GnW4ep" target="_blank">Word</a>, <time datetime="2020-10-26">26 oktober, 2020</time></cite></p>
       </div>
       <div class="col-md-5">
         <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 500x500"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"/><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg>
       </div>
     </div>
-
+    <!-- Metode valg slut -->
     <hr class="featurette-divider">
-
-    <div class="row featurette">
-      <div class="col-md-7 order-md-2">
-        <h2 class="featurette-heading">Oh yeah, it’s that good. <span class="text-muted">See for yourself.</span></h2>
-        <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
-      </div>
-      <div class="col-md-5 order-md-1">
-        <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 500x500"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"/><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg>
-      </div>
-    </div>
-
-    <hr class="featurette-divider">
-
+    <!-- Metode valg GANGSTER-TEST -->
     <div class="row featurette">
       <div class="col-md-7">
-        <h2 class="featurette-heading">And lastly, this one. <span class="text-muted">Checkmate.</span></h2>
-        <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
-      </div>
-      <div class="col-md-5">
-        <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 500x500"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"/><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg>
+        <h2 class="featurette-heading">Metode valg. <span class="sidenavn">Gangster-test.</span></h2>
+        <p class="lead">Gangster-test er en kvalitativ undersøgelses form. Vi valgte denne testform, da den danner et rigtig godt udgangspunkt at arbejde ud fra, da det er en test af websitets informationsarkitektur, den skal derfor sikre at de vigtigste hjælpemidler er på plads den siden, samt at siden overhoder de mest almindelige koventioner. Testen er ikke voldsom dybdegående men alligevel danner den et overblik som er vigtigt til videre udvikling, samt fortæller om man har ramt rigtig eller forkert når man snakker brugervenlighed.</p>
+        <p class="lead">Denne test fungere på en måde så der er 6 spørgsmål som brugeren skal fokusere på, under hvert punkt skal brugeren så give points fra 0-3</p>
+        <ul>
+          <li><span class="tyk">0 point:</span> Denne information fremgår slet ikke af undersiden</li>
+          <li><span class="tyk">1 point:</span> Denne information fremgår kun delvis af undersiden / svær at finde</li>
+          <li><span class="tyk">2 point:</span> Denne information kunne fremgå tydeligere på undersiden</li>
+          <li><span class="tyk">3 point:</span> Denne information fremgår tydeligt på undersiden. </li>
+        </ul>
+        <p class="lead">Dette ender ud i en mulig total på 0-18 point. hvor 0 er udtryk for, at navigationen er dårligt understøttet, og 18 er udtryk for, at navigationen er meget tydeligt og godt understøttet.</p>
+        <p class="lead">Testen starter med at spørge om hvilket website man er på, dette er meget væsentligt da det er vigtigt at kunne se hvilken side man befinder sig på, uden at skulle kigge på browserens søgefelt.</p>
+        <p class="lead">Andet spørgsmål ligger på om der er en ordentlig navigations menu på siden. Dette er vigtigt for brugervenligheden på siden, da det hjælper ens brugere med at finde frem til de ting som de søger.</p>
+        <p class="lead">Derefter bliver der spurgt til underside, dette felt er måske ikke lige så vigtigt for alle sider, men stadig en god ting at kunne se hvor man egentlig befinder sig, så man kan finde frem til de ting eller information igen.</p>
+        <p class="lead">Så bliver der spurgt til hvilke muligheder man har på siden, om der er undermenuer, links downloads osv.</p>
+        <p class="lead">Femte spørgsmål ligger vægt på om der er en ordentlig navigations vejledning som fortæller hvordan man fandt til siden, en såkaldt brødkrummesti.</p>
+        <p class="lead">Til slut bliver der spurgt til om siden har en søgefunktion og om hvor let den er at finde, hvis den er der. Det er dog ikke alle sider som har en søgefunktion, dette er ikke fordi de har overset det, men i stedet fordi det ikke er nødvendigt for sidens opsætning.</p>
+        <p class="lead"><cite>-Andreas, Veli og Jonas, Moodle/<a class="google" href="https://itcn-my.sharepoint.com/:w:/g/personal/10741_student_eadania_dk/EZBUH-M0lBdPt6lZy7cqm6QBg4g2nbzlip7QKr4vXX69Uw?e=GnW4ep" target="_blank">Word</a>, <time datetime="2020-10-26">26 oktober, 2020</time></cite></p>
       </div>
     </div>
-
+    <!-- Metode valg GANGSTER-TEST SLUT -->
+    <hr class="featurette-divider">
+    <!-- METODE VALG BRUGERTEST -->
+    <div class="row featurette">
+      <div class="col-md-7">
+        <h2 class="featurette-heading">Metode valg. <span class="sidenavn">Brugertest - Tænkhøjttesten.</span></h2>
+        <p class="lead">Dette er en form for bruger-test, ligesom andre bruger-tests er det en kvalitativ undersøgelses form. Den er mere åben en den almene bruger-test, da man ikke stille ligeså lukkede spørgsmål til den bruger man tester, det er en mere åben og observerende samtale an har med dem, det er dog vigtigt at man gør brugeren opmærksom på at de skal ”tænke høj”, dette vil sige at de gerne skal sige alle de ting de tænker under undersøgelsen, samt hvorfor de gør som de gør. Det er også MEGET VIGTIGT at man informere brugeren omkring hvad der skal ske under undersøgelsen, hvad den skal bruges til, og hvordan man opbevare den data man indsamler, samt for samtykke omkring mulig optagelser af undersøgelsen. Det er også vigtigt at brugeren forstår at det ikke er dem man tester og undersøger, men i stedet den hjemmeside eller det produkt som de er med til at teste.</p>
+        <p class="lead">Det er også en god ide at optage undersøgelsen med enten video(bedste form for optagelse) eller lyd, dette gør man da der er ting som man kan have overset under undersøgelsen, og ved hjælp af optagelserne kan finde tilbage, så opfanger man også alle brugerens handlinger og kropssprog. Det er også en fordel at være flere observanter, da man som mennesker ikke kan opfange al data selv, og skulle man have spørgsmål, så er det ikke sikkert at man også kan nå at opfange de ting som brugeren gør, både inden for analog og kropssprog.</p>
+        <p class="lead">Vi har brugt brugertest til at teste Teslas navigationsmuligheder på deres hjemmeside samt også Bilkas hjemmeside. Vi testede hjemmesiden på en bruger, altså en testperson. Brugertesten går ud på at man undersøger ens hjemmeside eller en valgt hjemmesides 3 vigtigste features. Når man så har fundet de 3 vigtigste features, så skal man til at formulere 3 opgaver på baggrund af de 3 features so man fokusere på. Disse opgaver er så noget som ens testperson skal lave, for at man skal kunne teste i forhold til f.eks. informationsarkitektur eller forståelighed for bruger. Brugertesten har meget tilfælles med ”tænke højt-testen”. Men Brugertesten har mere faste rammer, hvor dem der har lavet produktet forklare en del mere og guider mere brugeren igennem hjemmesiden f.eks.  Ved en ”tænk højt-test” giver du brugeren mere plads til ”lege” på hjemmesiden, altså brugeren får lov til at bevæge sig mere frivilligt rundt på den valgt hjemmeside.</p>
+        <p class="lead"><cite>-Andreas, Veli og Jonas, Moodle/<a class="google" href="https://itcn-my.sharepoint.com/:w:/g/personal/10741_student_eadania_dk/EZBUH-M0lBdPt6lZy7cqm6QBg4g2nbzlip7QKr4vXX69Uw?e=GnW4ep" target="_blank">Word</a>, <time datetime="2020-10-26">26 oktober, 2020</time></cite></p>
+      </div>
+    </div>
+    <!-- METODE VALG BRUGERTEST SLUT -->
+    <hr class="featurette-divider">
+    <!-- METODE VALG SPØRGESKEMA -->
+    <div class="row featurette">
+      <div class="col-md-7">
+        <h2 class="featurette-heading">Metode valg. <span class="sidenavn">Spørgeskema.</span></h2>
+        <p class="lead">Vi har valgt bruge spørgeskema som en metode til League of Legends hjemmeside. Et spørgeskema er en kvantitativ undersøgelse der består af mængde og data, som man kan aflæse bagefter. Spørgeskemaer bruger man til korte og konkrete svar. Man dykker ikke ned svarene, altså man spørger ikke om hvorfor, man spørger om noget man sige ja, eller nej til, eller land/region f.eks. </p>
+        <p class="lead"><cite>-Andreas, Veli og Jonas, Moodle/<a class="google" href="https://itcn-my.sharepoint.com/:w:/g/personal/10741_student_eadania_dk/EZBUH-M0lBdPt6lZy7cqm6QBg4g2nbzlip7QKr4vXX69Uw?e=GnW4ep" target="_blank">Word</a>, <time datetime="2020-10-26">26 oktober, 2020</time></cite></p>
+      </div>
+    </div>
+    <!-- METODE VALG SPØRGESKEMA SLUT -->
     <hr class="featurette-divider">
 
     <!-- /END THE FEATURETTES -->
@@ -108,10 +134,11 @@
       <i class="fab fa-instagram footer-icon"></i>
       <i class="fas fa-envelope footer-icon"></i>
 
-    <p>© Copyright 2020 Jonas Hawthorn Mikkelsen</p>
+    <p class="copyright">© Copyright 2020 Jonas Hawthorn Mikkelsen</p>
   </div>
   </footer>
 </main>
+<!-- JAVASCRIPT BRUGT TIL ALLE UNDERSIDER -->
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
       <script>window.jQuery || document.write('<script src="assets/js/vendor/jquery.slim.min.js"><\/script>')</script><script src="assets/dist/js/bootstrap.bundle.min.js"></script>
 </html>
