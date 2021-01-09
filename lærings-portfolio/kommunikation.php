fd2a7c9642916f850fcb182ce18785cadf98b0ab
Jhm20P <71630698+Jhm20P@users.noreply.github.com>
2021-01-09 19:57:30 +0100

    Update kommunikation.php

--- lærings-portfolio/kommunikation.php
+++ lærings-portfolio/kommunikation.php
@@ -1,12 +1,13 @@
 <!doctype html>
-<html lang="en">
+<html lang="da">
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
-    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="author" content="Jonas Hawthorn Mikkelsen">
     <meta name="generator" content="Jekyll v4.1.1">
-    <title>Album example · Bootstrap</title>
+    <link rel="icon" href="favicon.ico">
+    <title>Kommunikation</title>
 
     <!-- Javascript for iconer fra fontawesome -->
     <script src="https://kit.fontawesome.com/c43d39253a.js" crossorigin="anonymous"></script>
@@ -15,8 +16,9 @@
 
     <!-- Bootstrap core CSS -->
 <link href="assets/dist/css/bootstrap.min.css" rel="stylesheet">
+<!-- EGEN CSS -->
 <link rel="stylesheet" href="underside.css">
-
+<!-- INTERN CSS -->
     <style>
       .bd-placeholder-img {
         font-size: 1.125rem;
@@ -34,145 +36,140 @@
       }
     </style>
     <!-- Custom styles for this template -->
+    <!-- MENU BAR -->
     <?php include("Menu.inc"); ?>
 
-<main role="main">
+    <main role="main">
+<!-- SEKTION MED OVERSKRIFT OG UNDEROVERSKRIFT -->
+      <section class="jumbotron text-center">
+        <div class="container">
+          <h1 class="text-muted">Kommunikation</h1>
+          <p class="lead text-muted">Her kan man finde informationer omkring hvad jeg har lært angående Kommunikation blandt mennesker, og hvordan folks forståelse af ting kan bruges i forhold til et website</p>
+        </div>
+      </section>
 
-  <section class="jumbotron text-center">
-    <div class="container">
-      <h1>Kommunikation</h1>
-      <p class="lead text-muted">Her kan man finde informationer omkring hvad jeg har lært angående Kommunikation blandt mennesker, og hvordan folks forståelse af ting kan bruges i forhold til et website</p>
-    </div>
-  </section>
+      <div class="album py-5 bg-light">
+        <div class="container">
 
-  <div class="album py-5 bg-light">
-    <div class="container">
-
-      <div class="row">
-        <div class="col-md-4">
-          <div class="card mb-4 shadow-sm">
-            <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-            <div class="card-body">
-              <p class="card-text">Her vil jeg går ind over hvad det Grundlæggende vi har haft i kommunikation</p>
-              <div class="d-flex justify-content-between align-items-center">
-                <div class="btn-group">
-                  <a href="grundkommunikation.php"><button type="button" class="btn btn-sm btn-outline-secondary">Se mere</button></a>
+          <div class="row">
+            <!-- KVALITATIV OG KVANTITATIV -->
+            <div class="col-md-4">
+              <div class="card mb-4 shadow-sm">
+              <img class="card-img-top" src="kvankval.png" width="100%" height="225">
+                <div class="card-body">
+                  <p class="card-text">Her kommer jeg ind over hvad Kvalitativ og Kvalitativ test er.</p>
+                  <div class="d-flex justify-content-between align-items-center">
+                    <div class="btn-group">
+                    <a href="kvankval.php"><button type="button" class="btn btn-sm btn-outline-secondary">Se mere</button></a>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        </div>
-        <div class="col-md-4">
-          <div class="card mb-4 shadow-sm">
-            <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-            <div class="card-body">
-              <p class="card-text">Her kommer jeg ind over hvad Kvalitativ og Kvalitativ test er.</p>
-              <div class="d-flex justify-content-between align-items-center">
-                <div class="btn-group">
-                  <a href="kvankval.php"><button type="button" class="btn btn-sm btn-outline-secondary">Se mere</button></a>
+            <!-- PRÆSENTATIONSTEKNIK -->
+            <div class="col-md-4">
+              <div class="card mb-4 shadow-sm">
+                <img class="card-img-top" src="præsentation.png" width="100%" height="225">
+                <div class="card-body">
+                  <p class="card-text">Her kommer jeg ind over hvad Præsentationsteknik er.</p>
+                  <div class="d-flex justify-content-between align-items-center">
+                    <div class="btn-group">
+                    <a href="præsentation.php"><button type="button" class="btn btn-sm btn-outline-secondary">Se mere</button></a>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        </div>
-        <div class="col-md-4">
-          <div class="card mb-4 shadow-sm">
-            <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-            <div class="card-body">
-              <p class="card-text">Her kommer jeg ind over hvad Præsentationsteknik er.</p>
-              <div class="d-flex justify-content-between align-items-center">
-                <div class="btn-group">
-                  <a href="præsentation.php"><button type="button" class="btn btn-sm btn-outline-secondary">Se mere</button></a>
+            <!-- USABILITY -->
+            <div class="col-md-4">
+              <div class="card mb-4 shadow-sm">
+                <img class="card-img-top" src="usability.png" width="100%" height="225">
+                <div class="card-body">
+                  <p class="card-text">Her kommer jeg ind på hvad Usabillity test er, og hvad det kan bruges til.</p>
+                  <div class="d-flex justify-content-between align-items-center">
+                    <div class="btn-group">
+                      <a href="usabillity.php"><button type="button" class="btn btn-sm btn-outline-secondary">Se mere</button></a>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        </div>
-
-        <div class="col-md-4">
-          <div class="card mb-4 shadow-sm">
-            <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-            <div class="card-body">
-              <p class="card-text">Her kommer jeg ind på hvad Usabillity test er, og hvad det kan bruges til.</p>
-              <div class="d-flex justify-content-between align-items-center">
-                <div class="btn-group">
-                  <a href="usabillity.php"><button type="button" class="btn btn-sm btn-outline-secondary">Se mere</button></a>
+            <!-- TEKSTPRODUKTION -->
+            <div class="col-md-4">
+              <div class="card mb-4 shadow-sm">
+                <img class="card-img-top" src="tekst.png" width="100%" height="225">
+                <div class="card-body">
+                  <p class="card-text">Her vil jeg komme ind på hvad Tekstproduktion er og hvordan det laves.</p>
+                  <div class="d-flex justify-content-between align-items-center">
+                    <div class="btn-group">
+                    <a href="tekstproduktion.php"><button type="button" class="btn btn-sm btn-outline-secondary">Se mere</button></a>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        </div>
-        <div class="col-md-4">
-          <div class="card mb-4 shadow-sm">
-            <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-            <div class="card-body">
-              <p class="card-text">Her vil jeg komme ind på hvad Tekstproduktion er og hvordan det laves.</p>
-              <div class="d-flex justify-content-between align-items-center">
-                <div class="btn-group">
-                  <a href="tekstproduktion.php"><button type="button" class="btn btn-sm btn-outline-secondary">Se mere</button></a>
+            <!-- PARADIGMER -->
+            <div class="col-md-4">
+              <div class="card mb-4 shadow-sm">
+                <img class="card-img-top" src="paradigmer.png" width="100%" height="225">
+                <div class="card-body">
+                  <p class="card-text">Her vil jeg komme ind på de forskellige paradigmer og hvad de betyder hver især</p>
+                  <div class="d-flex justify-content-between align-items-center">
+                    <div class="btn-group">
+                      <a href="paradigmer.php"><button type="button" class="btn btn-sm btn-outline-secondary">Se mere</button></a>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        </div>
-        <div class="col-md-4">
-          <div class="card mb-4 shadow-sm">
-            <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-            <div class="card-body">
-              <p class="card-text">Her vil jeg komme ind på de forskellige paradigmer og hvad de betyder hver især</p>
-              <div class="d-flex justify-content-between align-items-center">
-                <div class="btn-group">
-                  <a href="paradigmer.php"><button type="button" class="btn btn-sm btn-outline-secondary">Se mere</button></a>
+            <!-- OPHAVSRET OG LICENS -->
+            <div class="col-md-4">
+              <div class="card mb-4 shadow-sm">
+                <img class="card-img-top" src="ophav.png" width="100%" height="225">
+                <div class="card-body">
+                  <p class="card-text">Her vil jeg komme ind på hvad Ophavsret er og hvilken betydning det kan have.</p>
+                  <div class="d-flex justify-content-between align-items-center">
+                    <div class="btn-group">
+                      <a href="ophavsret.php"><button type="button" class="btn btn-sm btn-outline-secondary">Se mere</button></a>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        </div>
-
-        <div class="col-md-4">
-          <div class="card mb-4 shadow-sm">
-            <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-            <div class="card-body">
-              <p class="card-text">Her vil jeg komme ind på hvad Ophavsret er og hvilken betydning det kan have.</p>
-              <div class="d-flex justify-content-between align-items-center">
-                <div class="btn-group">
-                  <a href="ophavsret.php"><button type="button" class="btn btn-sm btn-outline-secondary">Se mere</button></a>
+            <!-- BMC -->
+            <div class="col-md-4">
+              <div class="card mb-4 shadow-sm">
+                <img class="card-img-top" src="buisness.png" width="100%" height="225">
+                <div class="card-body">
+                  <p class="card-text">Her vil jeg komme ind på hvad BMC er og hvordan den bruges.</p>
+                  <div class="d-flex justify-content-between align-items-center">
+                    <div class="btn-group">
+                      <a href="bmc.php"><button type="button" class="btn btn-sm btn-outline-secondary">Se mere</button></a>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        </div>
-        <div class="col-md-4">
-          <div class="card mb-4 shadow-sm">
-            <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-            <div class="card-body">
-              <p class="card-text">Her vil jeg komme ind på hvad BMC er og hvordan den bruges.</p>
-              <div class="d-flex justify-content-between align-items-center">
-                <div class="btn-group">
-                  <a href="bmc.php"><button type="button" class="btn btn-sm btn-outline-secondary">Se mere</button></a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="col-md-4">
-          <div class="card mb-4 shadow-sm">
-            <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-            <div class="card-body">
-              <p class="card-text">Her vil jeg komme ind på  hvad Informationsarkitektur er og hvad det kan være godt for.</p>
-              <div class="d-flex justify-content-between align-items-center">
-                <div class="btn-group">
-                  <a href="informationsark.php"><button type="button" class="btn btn-sm btn-outline-secondary">Se mere</button></a>
+            <!-- INFORMATIONSARKITEKTUR -->
+            <div class="col-md-4">
+              <div class="card mb-4 shadow-sm">
+                <img class="card-img-top" src="infoark.png" width="100%" height="225">
+                <div class="card-body">
+                  <p class="card-text">Her vil jeg komme ind på  hvad Informationsarkitektur er og hvad det kan være godt for.</p>
+                  <div class="d-flex justify-content-between align-items-center">
+                    <div class="btn-group">
+                      <a href="informationsark.php"><button type="button" class="btn btn-sm btn-outline-secondary">Se mere</button></a>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  </div>
 
-</main>
+
+    </main>
 
 <footer class="white-section" id="footer">
 <div class="container-fluid">
@@ -181,9 +178,10 @@
     <i class="fab fa-instagram footer-icon"></i>
     <i class="fas fa-envelope footer-icon"></i>
 
-  <p>© Copyright 2020 Jonas Hawthorn Mikkelsen</p>
+  <p class="copyright">© Copyright 2020 Jonas Hawthorn Mikkelsen</p>
 </div>
 </footer>
+<!-- JAVASCRIPT BRUGT TIL ALLE UNDERSIDER -->
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
       <script>window.jQuery || document.write('<script src="assets/js/vendor/jquery.slim.min.js"><\/script>')</script><script src="assets/dist/js/bootstrap.bundle.min.js"></script>
 </html>
