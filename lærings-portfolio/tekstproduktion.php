69b11360cf0cfffb68f89759b5d18feaac168d03
Jhm20P <71630698+Jhm20P@users.noreply.github.com>
2021-01-09 14:42:51 +0100

    Update tekstproduktion.php

--- lærings-portfolio/tekstproduktion.php
+++ lærings-portfolio/tekstproduktion.php
@@ -47,7 +47,7 @@
   <section class="jumbotron text-center">
     <div class="container">
       <h1>Tekstproduktion</h1>
-      <p class="lead text-muted">Her kan man finde informationer omkring hvad jeg har lært angående design</p>
+      <p class="lead text-muted">Her kan man finde informationer omkring hvad jeg har lært angående tekstproduktion</p>
     </div>
   </section>
 
