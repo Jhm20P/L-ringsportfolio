499c083c9d80c47149856b97f0bb7edc7e19c7a6
Jhm20P <71630698+Jhm20P@users.noreply.github.com>
2021-01-09 16:57:05 +0100

    Update ophavsret.php

--- lærings-portfolio/ophavsret.php
+++ lærings-portfolio/ophavsret.php
@@ -1,12 +1,13 @@
 <!doctype html>
-<html lang="en">
+<html lang="da">
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
-    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="author" content="Jonas Hawthorn Mikkelsen">
     <meta name="generator" content="Jekyll v4.1.1">
-    <title>Carousel Template · Bootstrap</title>
+    <link rel="icon" href="favicon.ico">
+    <title>Ophavsret og Licens</title>
 
     <link rel="canonical" href="https://getbootstrap.com/docs/4.5/examples/carousel/">
 
@@ -15,9 +16,10 @@
 
     <!-- Bootstrap core CSS -->
 <link href="assets/dist/css/bootstrap.min.css" rel="stylesheet">
+  <!-- EGEN CSS -->
 <link rel="stylesheet" href="underside.css">
 <link rel="stylesheet" href="bodyfix.css">
-
+  <!-- INTERN CSS -->
     <style>
       .bd-placeholder-img {
         font-size: 1.125rem;
@@ -37,14 +39,15 @@
     <!-- Custom styles for this template -->
     <link href="carousel.css" rel="stylesheet">
   </head>
+    <!-- MENU BAR -->
   <?php include("Menu.inc"); ?>
 
 <main role="main">
-
+<!-- SEKTION MED OVERSKRIFT OG UNDEROVERSKRIFT -->
   <section class="jumbotron text-center">
     <div class="container">
       <h1>Ophavsret og Licens</h1>
-      <p class="lead text-muted">Her kan man finde informationer omkring hvad jeg har lært angående design</p>
+      <p class="lead text-muted">Her kan man finde informationer omkring hvad jeg har lært angående ophavsret og licens</p>
     </div>
   </section>
 
@@ -58,43 +61,180 @@
     <!-- START THE FEATURETTES -->
 
     <hr class="featurette-divider">
-
+    <!-- LOVGIVNINGEN -->
     <div class="row featurette">
       <div class="col-md-7">
-        <h2 class="featurette-heading">First featurette heading. <span class="text-muted">It’ll blow your mind.</span></h2>
-        <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
-      </div>
-      <div class="col-md-5">
-        <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 500x500"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"/><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg>
+        <h2 class="featurette-heading">Lovgivningen.</h2>
+        <p class="lead">Ophavsret er også kendt som ”Copyright” på engelsk. Copyright har symbolet ©.  Det som ophavsretten gør, er at den beskytter folks værker fra at blive ”taget” fra dem. Det gælder film, musik, billeder, malerier og mange andre produkter som der er patent på.  Den eneste grund til at en anden person må have lov til at bruge et bestemt værk, er hvis skaberen har givet en lov til det. Ophavsretten er for det meste gældende i 70 år eller til at skaberen af værket er død. Dog er det kun 50 år for musik værker.</p>
+        <p class="lead"><cite>- Frederik Kloosterman og Jonas Hawthorn MIkkelsen, HTX Kom/it, <a class="google" href="https://docs.google.com/document/d/1911OvOgwkAHJAN4hp2BQ4lPhjqn7LLSHHKm_xUORJJ4/edit?usp=sharing" target="_blank">Google Docs</a>, <time datetime="2019-11-28">28 November 2019</time></cite></p>
       </div>
     </div>
-
+    <!-- LOVGIVNINGEN SLUT -->
     <hr class="featurette-divider">
-
+    <!-- PERSONDATALOVEN -->
     <div class="row featurette">
       <div class="col-md-7 order-md-2">
-        <h2 class="featurette-heading">Oh yeah, it’s that good. <span class="text-muted">See for yourself.</span></h2>
-        <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
-      </div>
-      <div class="col-md-5 order-md-1">
-        <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 500x500"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"/><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg>
+        <h2 class="featurette-heading">Persondataloven / GDPR. <span class="sidenavn">Redegør for persondataloven / GDPR.</span></h2>
+        <p class="lead">Alle virksomheder uanset lille eller stor, er omfattet af databeskyttelsesforordningen/GDPR når en virksomhed håndtere enhver form for data eller personlige oplysninger hos en person eller en medarbejder. <i class="sidenavn">“Som de vigtigste former for behandling kan nævnes: Indsamling, registrering, data-klassificering, opbevaring, brug, videregivelse, samkøring og sletning.”</i> (Revi-it, Persondata, linje 2, 03-11-20)</p>
+        <p class="lead">Når det kommer til virksomhedernes behandling af medarbejders løn samt HR, Medfølger det naturligvis at man skal følge kravene i databeskyttelsesforordningen/GDPR. Fleste tilfælde skal virksomheder foretage en andmeldelse til Datatilsynet angående deres registrering samt anvendelse af data.</p>
+        <p class="lead"><i class="sidenavn">Visse af lovens regler gælder endvidere også for, hvordan I må behandle oplysninger om virksomheder, foreninger og lignende.”</i> (Revi-it, Persondata, linje 7, 03-11-20)</p>
+        <p class="lead">I 2018 den 25 maj, skete der nogle ændringer samt nogle skærpelser angående den ikraftsættelse for de krav som er i data protection officer, på dansk Databeskyttelserådgiver, som sørger for retten til at blive glemt, og indberetningspligt.</p>
+        <p class="lead"><cite>-Jonas, Moodle/<a class="google" href="https://docs.google.com/document/d/1cf4uUIAf-DMX4gJiBn6FedtsNJDmqwqMJ4RR8NNLi5U/edit?usp=sharing" target="_blank">Google docs</a>, <time datetime="2020-11-03">3 November, 2020</time></cite></p>
       </div>
     </div>
-
+    <!-- PERSONDATALOVEN SLUT -->
     <hr class="featurette-divider">
-
+    <!-- IMATERIELLE RETTIGHEDER 1. SEKTION -->
     <div class="row featurette">
       <div class="col-md-7">
-        <h2 class="featurette-heading">And lastly, this one. <span class="text-muted">Checkmate.</span></h2>
-        <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
-      </div>
-      <div class="col-md-5">
-        <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 500x500"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"/><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg>
+        <h2 class="featurette-heading">Immaterielle rettigheder. <span class="sidenavn">Redegør for hvad immaterielle rettigheder er.</span></h2>
+        <p class="lead">Immaterielle rettigheder, altså  en intellektuel ejendomsret, er en fællesbetegnelse for retsbeskyttelsen for intellektuelle frembringelser. Det betyder at det f.eks. kan være Kunst, Billeder, videoer, film, lyd, litteratur, opfindelser, formgivning design samt forretningskendetegn. Det er lovbestemt at der en beskyttelse af immaterielle rettigheder.</p>
+        <p class="lead">Der er forskellige love når det kommer til de forskellige frembringelser, der skal beskyttes. Disse forskellige love nævnes tit i eneretslovene og har visse fællestræk</p>
+        <p class="lead"><i class="sidenavn">“Det er navnlig ophavsretsloven, patentloven, brugsmodelloven, designloven og varemærkeloven.”</i> (Jura-guide, immaterielle-rettigheder, linje 7, 03-11-20)</p>
+        <p class="lead"><cite>-Jonas, Moodle/<a class="google" href="https://docs.google.com/document/d/1cf4uUIAf-DMX4gJiBn6FedtsNJDmqwqMJ4RR8NNLi5U/edit?usp=sharing" target="_blank">Google docs</a>, <time datetime="2020-11-03">3 November, 2020</time></cite></p>
       </div>
     </div>
-
+    <!-- IMATERIELLE RETTIGHEDER 1. SEKTION SLUT -->
     <hr class="featurette-divider">
-
+    <!-- IMATERIELLE RETTIGHEDER 2. SEKTION -->
+    <div class="row featurette">
+      <div class="col-md-7 order-md-2">
+        <h2 class="featurette-heading">Immaterielle rettigheder. <span class="sidenavn">Redegør for ophavsret.</span></h2>
+        <p class="lead">Ophavsretten er det der sørger for at folk ikke kan tage/stjæle noget af det du har lavet, så længe det du har lavet er originalt, altså at du selv har lavet det fra bunden. Det kan også være at du har taget noget fra nogle andre i forhold til noget musik, men det meste af beaten i det musik du udgiver er dit, så har du stadig ophavsret på det.</p>
+        <p class="lead"> I Danmark har vi ophavsrets paragrafferne §1 som er beskyttelse af værker, §66 er om lyd, §67 er om billeder og til sidst er der §70 som er fotografier for interesse. Der er selvfølgelig også andre ting som tekster, skulpturer, malerier, covers samt lignende værker der er beskyttet. Ens kreationer som man selv har skabt har automatisk ophavsret, selvom man ikke gøre folk klar over det ved at f.eks. bruge et copyright tegn på ens hjemmeside.</p>
+        <p class="lead">Skal man altid spørge om lov før man bruger en andens værk?</p>
+        <ul>
+          <li>Ophavsretten også kendt som det engelske ord “Copyright” og det har symbolet: ©.
+      Det ophavsretten betyder, er at man ikke må tage værker, såsom film, musik, billeder og
+      malerier osv. fra dem der har skabt det, med mindre de giver én lov til at bruge det.
+      Denne  rettighed gælder generelt for 70 år eller til at skaberen er død men kun 50 år når
+      det gælder musik. Det vil sige at rock’n roll fra 1950’erne kan blive brugt frit i dag, dog
+      ved billeder kan man bruge alle billeder der blev taget før 1970.</li>
+        </ul>
+        <p class="lead">Kom specielt ind på ophavsretten på sociale medie, herunder:</p>
+        <ul>
+          <li>Brugen af værker fra sociale medier
+            <ul>
+              <li>Når det kommer til sociale medier gælder de samme regler som for alle andre punkter i ophavsretsloven.  Bruger du Facebook eller Twitter skal man være opmærksom på at man skal overholde reglerne for upload samt linking. F.eks. må du ikke lægge billeder eller musik op på de sociale medier som andre har rettighederne på, altså ophavsretten på.  Du skal også overholde downloading og streaming af materiale som du bruger, men bliver delt af andre, det skal dog siges at det er ulovligt at downloade eller streame materiale som folk har delt ulovligt. Du skal dog være opmærksom på at når du opretter en Facebook, så giver du Facebook en licens til at bruge dine billeder og videoer.</li>
+            </ul>
+          </li>
+          <li>Linking til værker på sociale medier
+            <ul>
+              <li>I Danmark har vi ikke nogen specifikke love imod linking. Men i domstolene er det blevet slået fast, at det er ulovligt at linke til musiknumre der var lagt op på sted som rettighedshaverne ikke har givet samtykke til.</li>
+            </ul>
+          </li>
+        </ul>
+        <p class="lead"><cite>-Jonas, Moodle/<a class="google" href="https://docs.google.com/document/d/1cf4uUIAf-DMX4gJiBn6FedtsNJDmqwqMJ4RR8NNLi5U/edit?usp=sharing" target="_blank">Google docs</a>, <time datetime="2020-11-03">3 November, 2020</time></cite></p>
+      </div>
+    </div>
+    <!-- IMATERIELLE RETTIGHEDER 2. SEKTION SLUT -->
+    <hr class="featurette-divider">
+    <!-- IMATERIELLE RETTIGHEDER 3. SEKTION -->
+    <div class="row featurette">
+      <div class="col-md-7">
+        <h2 class="featurette-heading">Immaterielle rettigheder. <span class="sidenavn">Forklar/redegør også følgende.</span></h2>
+        <ul>
+          <li>Hvad er et varemærke
+            <ul>
+              <li>Det der kendetegner et varemærke er at det er et særligt navn eller kendetegn erhvervsdrivende, altså virksomheder bruger til at markedsføre sig selv eller de produkter de prøver at sælge.</li>
+            </ul>
+          </li>
+          <li>Hvad betyder varemærkeloven for dig som multimediedesigner?
+            <ul>
+              <li>Som multimediedesigner kan det betyde at jeg ikke kan bruge nogle ting på nettet, medmindre der er en licens til som betaler til, eller man køber ophavsretten til det bestemte varemærker.</li>
+            </ul>
+          </li>
+          <li>Kan en idé beskyttes af ophavsretten?
+            <ul>
+              <li>Ja, du kan vælge at tage patent på idéen inden du begynder at lave det om til et produkt.</li>
+            </ul>
+          </li>
+        </ul>
+        <p class="lead">Der er forskellige love når det kommer til de forskellige frembringelser, der skal beskyttes. Disse forskellige love nævnes tit i eneretslovene og har visse fællestræk</p>
+        <p class="lead"><i class="sidenavn">“Det er navnlig ophavsretsloven, patentloven, brugsmodelloven, designloven og varemærkeloven.”</i> (Jura-guide, immaterielle-rettigheder, linje 7, 03-11-20)</p>
+        <p class="lead"><cite>-Jonas, Moodle/<a class="google" href="https://docs.google.com/document/d/1cf4uUIAf-DMX4gJiBn6FedtsNJDmqwqMJ4RR8NNLi5U/edit?usp=sharing" target="_blank">Google docs</a>, <time datetime="2020-11-03">3 November, 2020</time></cite></p>
+      </div>
+    </div>
+    <!-- IMATERIELLE RETTIGHEDER 3. SEKTION SLUT -->
+    <hr class="featurette-divider">
+    <!-- LICENSAFTALER -->
+    <div class="row featurette">
+      <div class="col-md-7 order-md-2">
+        <h2 class="featurette-heading">Hvad er licensaftaler?. <span class="sidenavn">Redegør for ophavsret.</span></h2>
+        <ul>
+          <li>Redegør for licensaftaler
+            <ul>
+              <li>En licensaftale er en aftale der foregår mellem to personer eller to virksomheder, hvor at den der har ophavsretten på “produktet”, giver den anden part lov til at bruge det “produkt” i mod betaling.</li>
+            </ul>
+          </li>
+          <li>Der findes et Open Source JavaScript Framework kaldet “jQuery”. Hvilken licens er det udgivet under?
+            <ul>
+              <li>MIT Licens
+                <ul>
+                  <li>Beskriv kort de restriktioner og friheder som denne licens giver
+                    <ul>
+                      <li>Der stortset ingen restriktioner, du kan frit bruge det til komerciel brug så længe at du beholder Copyright Headeren.</li>
+                    </ul>
+                  </li>
+                </ul>
+              </li>
+            </ul>
+          </li>
+        </ul>
+        <p class="lead"><cite>-Jonas, Moodle/<a class="google" href="https://docs.google.com/document/d/1cf4uUIAf-DMX4gJiBn6FedtsNJDmqwqMJ4RR8NNLi5U/edit?usp=sharing" target="_blank">Google docs</a>, <time datetime="2020-11-03">3 November, 2020</time></cite></p>
+      </div>
+    </div>
+    <!-- LICENSAFTALER SLUT -->
+    <hr class="featurette-divider">
+    <!-- CREATIVE COMMONS -->
+    <div class="row featurette">
+      <div class="col-md-7">
+        <h2 class="featurette-heading">Creative Commons. <span class="sidenavn">Forklar/redegør også følgende.</span></h2>
+        <ul>
+          <li> Beskriv hvad Creative Commons er
+            <ul>
+              <li>Creative commons er en organisation der viser sig at være nyttig over for Musikere, kunstnere, forfattere samt andre rettighedshavere. Dette gør at de sikkert, kan dele deres ting med andre.  Med creative commons kan rettighedshaverne gøre sådan at andre kan kopier deres værker/produkter som det lyster og forære dem væk, så længe at man kreditere rettighedshaverne samt at man ikke bruger deres værker til kommercielt brug.</li>
+            </ul>
+          </li>
+          <li>Redegør for de seks licensering former der findes under Creative Commons
+            <ul>
+              <li>Navngivelse(by)
+                <ul>
+                  <li>Ved denne licens tillader man at andre kan distribuere dit værk, som ændre på det samt tilpasse det, og bruge det kommerciel, så længe at de kreditere dig(rettighedshaverne)</li>
+                </ul>
+              </li>
+              <li>Del på samme vilkår(Share-alike)
+                <ul>
+                  <li>Den her licens tillader at andre kan bearbejde, hvilket medfølger at de kan tilpasse samt ændre dit værk samt bruge det kommerciel sigte, så længe at de kreditere dig og licensere det nye værk på samme vilkår.</li>
+                </ul>
+              </li>
+              <li>Navngivelse - ingen bearbejdelser
+                <ul>
+                  <li>Den her licens tillader at man kan sprede værker til offentligheden kommerciel såvel som ikke-kommerciel, på baggrund af at værket distribueres uændret med kreditering til rettighedshaveren</li>
+                </ul>
+              </li>
+              <li>Navngivelse-ikke-kommerciel
+                <ul>
+                  <li>Den her licens tillader at andre kan bearbejde, hvilket medfølger at de kan tilpasse samt ændre dit værk, dog med ikke kommerciel sigte. Der skal stadig krediteres til dig ved de nye værke samt at de nye værker må ikke bruges kommerciel. Dog er det ikke nødvendigt at der bliver licenseret under samme vilkår.</li>
+                </ul>
+              </li>
+              <li>Navngivelse-ikke-kommerciel-Del på samme vilkår
+                <ul>
+                  <li>Den her licens tillader at andre kan bearbejde, samt tilpasse og ændre dit værk sålænge at de ikke bruger det kommercielt, på betingelse af at de kreditere dig samt licensere det nye værk på lignende vilkår. Andre kan dog downloade samt sprede dit værk til offentligheden ligesom ved den sidste licens. Men de må dog gerne oversætte, ændre forfatte nye værker på baggrund af dit værk. Alle nye værker der er blevet lavet på baggrund af dit, skal licenseres på lige vis.</li>
+                </ul>
+              </li>
+              <li>Navngivelse-ikke-kommerciel- ingen bearbejdelser
+                <ul>
+                  <li>Den her licens tillader at folk kan sprede dine værker på offentligheden, så længe at de kreditere samt linker tilbage til dig.</li>
+                </ul>
+              </li>
+            </ul>
+        </ul>
+        <p class="lead"><cite>-Jonas, Moodle/<a class="google" href="https://docs.google.com/document/d/1cf4uUIAf-DMX4gJiBn6FedtsNJDmqwqMJ4RR8NNLi5U/edit?usp=sharing" target="_blank">Google docs</a>, <time datetime="2020-11-03">3 November, 2020</time></cite></p>
+      </div>
+    </div>
+    <!-- CREATIVE COMMONS SLUT -->
+    <hr class="featurette-divider">
     <!-- /END THE FEATURETTES -->
 
   </div><!-- /.container -->
@@ -108,10 +248,11 @@
       <i class="fab fa-instagram footer-icon"></i>
       <i class="fas fa-envelope footer-icon"></i>
 
-    <p>© Copyright 2020 Jonas Hawthorn Mikkelsen</p>
+    <p class="copyright">© Copyright 2020 Jonas Hawthorn Mikkelsen</p>
   </div>
   </footer>
 </main>
+<!-- JAVASCRIPT BRUGT TIL ALLE UNDERSIDER -->
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
       <script>window.jQuery || document.write('<script src="assets/js/vendor/jquery.slim.min.js"><\/script>')</script><script src="assets/dist/js/bootstrap.bundle.min.js"></script>
 </html>
