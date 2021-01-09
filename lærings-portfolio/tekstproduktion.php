44396daf5ef3abbe26ae808f2e4b742862f043ea
Jhm20P <71630698+Jhm20P@users.noreply.github.com>
2021-01-09 14:41:01 +0100

    Update tekstproduktion.php

--- lærings-portfolio/tekstproduktion.php
+++ lærings-portfolio/tekstproduktion.php
@@ -1,12 +1,13 @@
 <!doctype html>
-<html lang="en">
+<html lang="da">
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
-    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="author" content="Jonas Hawthorn Mikkelsen">
     <meta name="generator" content="Jekyll v4.1.1">
-    <title>Carousel Template · Bootstrap</title>
+    <link rel="icon" href="favicon.ico">
+    <title>Tekstproduktion</title>
 
     <link rel="canonical" href="https://getbootstrap.com/docs/4.5/examples/carousel/">
 
@@ -15,9 +16,10 @@
 
     <!-- Bootstrap core CSS -->
 <link href="assets/dist/css/bootstrap.min.css" rel="stylesheet">
+  <!-- EGEN CSS -->
 <link rel="stylesheet" href="underside.css">
 <link rel="stylesheet" href="bodyfix.css">
-
+  <!-- INTERN CSS -->
     <style>
       .bd-placeholder-img {
         font-size: 1.125rem;
@@ -37,10 +39,11 @@
     <!-- Custom styles for this template -->
     <link href="carousel.css" rel="stylesheet">
   </head>
+    <!-- MENU BAR -->
   <?php include("Menu.inc"); ?>
 
 <main role="main">
-
+  <!-- SEKTION MED OVERSKRIFT OG UNDEROVERSKRIFT -->
   <section class="jumbotron text-center">
     <div class="container">
       <h1>Tekstproduktion</h1>
@@ -58,41 +61,67 @@
     <!-- START THE FEATURETTES -->
 
     <hr class="featurette-divider">
-
+    <!-- 7 GENIALE GENVEJE -->
     <div class="row featurette">
       <div class="col-md-7">
-        <h2 class="featurette-heading">First featurette heading. <span class="text-muted">It’ll blow your mind.</span></h2>
-        <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
-      </div>
-      <div class="col-md-5">
-        <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 500x500"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"/><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg>
+        <h2 class="featurette-heading">7 geniale genveje.</h2>
+        <!-- LIST AF SVAR -->
+        <ol>
+          <li> Gode overskrifter
+            <ul>
+              <li>
+                Den første del af overskriften kommer med “hun gør det igen” ligesom en indledende del af overskriften for at fange læseren. Det næste der kommer i overskriften er noget mere specifikt, altså der bliver beskrevet hvem det er der går det igen, og i dette tilfælde handler det om Trine dyrholm som er suveræn i ny krigsdrama. Den anden del af overskriften hjælper til at læseren får mere lyst til at læse teksten da de for at vide hvem artiklen handler om.
+              </li>
+            </ul>
+          </li>
+          <li>Et par linjers resumé
+            <ul>
+              <li>
+                I det som man kan kalde resuméet af artiklen bliver der beskrevet hvordan vi som læsere ikke skal misfortolke “Erna i krig” i den forstand at vi ikke skal tolke det som et kortspil, eller en gammel dame der belære soldater gode manérer. Men hellere på at det en mor på 40 år som kæmper for at hendes søn ikke skal blive dræbt i krig.
+              </li>
+            </ul>
+          </li>
+          <li>Afsnit med underoverskrifter
+            <ul>
+              <li>
+                Underoverskrifterne kan godt virke lidt forvirrende når man bare lige drager blikket ned på dem, men når man har læst de første to linjer under hver underoverskrift, kan man se at det giver mening med den underoverskrift.
+              </li>
+            </ul>
+          </li>
+          <li>Kun én idé/ét emne for hvert afsnit
+            <ul>
+              <li>
+                Det hele handler ligesom om Trine Dyrholm i den her film. Hvor ved den første underoverskrift bliver der forklaret hvor godt Trine har gjort det i filmen, og i den næste underoverskrift bliver der talt om hvor godt Ulrich Thomsen har gjort det.
+              </li>
+            </ul>
+          </li>
+          <li>Fremhævninger af nøgleord
+            <ul>
+              <li>
+                Der bliver ikke brugt andre fremhævninger end ved brug “”. Det kan ses flere gange med “Erna i krig” eller med “sandheden.
+              </li>
+            </ul>
+          </li>
+          <li>Punktopstilling
+            <ul>
+              <li>
+                Under billedet til overskriften, kan man se en lille boks hvor der er lavet en punktopstilling i. Den har punktopstilling beskriver hvem der er instruktør, hvem de medvirkende er og hvornår den her krigsdrama har premiere.
+              </li>
+            </ul>
+          </li>
+          <li>Meningsfulde links
+            <ul>
+              <li>
+                Lige inden selve brødteksten begynder er der tre funktioner man kan vælge. Man kan blandt andet vælge imellem at få forklaret nogle svære ord i teksten, ændre skriftstørrelsen eller man kan vælge at få læst artiklen op. Også nede i bunden er der link til Facebook, Twitter samt man kan kopier  linket.
+              </li>
+            </ul>
+          </li>
+        </ol>
+        <!-- LIST AF SVAR SLUT -->
+        <p class="lead"><cite>- Jonas, Moodle/<a href="https://docs.google.com/document/d/17-U52P5H9Iw0gs_6EzjCEw5Gk9Nol4svJGljAtOJdi0/edit" target="_blank">Google docs</a>, <time datetime"2020-10-30">30 Oktober 2020</time></cite></p>
       </div>
     </div>
-
-    <hr class="featurette-divider">
-
-    <div class="row featurette">
-      <div class="col-md-7 order-md-2">
-        <h2 class="featurette-heading">Oh yeah, it’s that good. <span class="text-muted">See for yourself.</span></h2>
-        <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
-      </div>
-      <div class="col-md-5 order-md-1">
-        <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 500x500"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"/><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg>
-      </div>
-    </div>
-
-    <hr class="featurette-divider">
-
-    <div class="row featurette">
-      <div class="col-md-7">
-        <h2 class="featurette-heading">And lastly, this one. <span class="text-muted">Checkmate.</span></h2>
-        <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
-      </div>
-      <div class="col-md-5">
-        <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 500x500"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"/><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg>
-      </div>
-    </div>
-
+    <!-- 7 GENIALE GENVEJE SLUT -->
     <hr class="featurette-divider">
 
     <!-- /END THE FEATURETTES -->
@@ -108,10 +137,11 @@
       <i class="fab fa-instagram footer-icon"></i>
       <i class="fas fa-envelope footer-icon"></i>
 
-    <p>© Copyright 2020 Jonas Hawthorn Mikkelsen</p>
+    <p class="copyright">© Copyright 2020 Jonas Hawthorn Mikkelsen</p>
   </div>
   </footer>
 </main>
+<!-- JAVASCRIPT BRUGT TIL ALLE UNDERSIDER -->
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
       <script>window.jQuery || document.write('<script src="assets/js/vendor/jquery.slim.min.js"><\/script>')</script><script src="assets/dist/js/bootstrap.bundle.min.js"></script>
 </html>
