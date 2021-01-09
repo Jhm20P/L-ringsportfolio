7722eb45372944981aa21c7c23a3ab608ea857b2
Jhm20P <71630698+Jhm20P@users.noreply.github.com>
2021-01-09 17:02:21 +0100

    Update bmc.php

--- lærings-portfolio/bmc.php
+++ lærings-portfolio/bmc.php
@@ -1,12 +1,13 @@
 <!doctype html>
-<html lang="en">
+<html lang="da">
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
-    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="author" content="Jonas Hawthorn Mikkelsen">
     <meta name="generator" content="Jekyll v4.1.1">
-    <title>Carousel Template · Bootstrap</title>
+    <link rel="icon" href="favicon.ico">
+    <title>BMC</title>
 
     <link rel="canonical" href="https://getbootstrap.com/docs/4.5/examples/carousel/">
 
@@ -15,9 +16,11 @@
 
     <!-- Bootstrap core CSS -->
 <link href="assets/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- EGEN CSS -->
 <link rel="stylesheet" href="underside.css">
 <link rel="stylesheet" href="bodyfix.css">
-
+<link rel="stylesheet" href="billede.css">
+  <!-- INTERN CSS -->
     <style>
       .bd-placeholder-img {
         font-size: 1.125rem;
@@ -37,14 +40,15 @@
     <!-- Custom styles for this template -->
     <link href="carousel.css" rel="stylesheet">
   </head>
+    <!-- MENU BAR -->
   <?php include("Menu.inc"); ?>
 
 <main role="main">
-
+<!-- SEKTION MED OVERSKRIFT OG UNDEROVERSKRIFT -->
   <section class="jumbotron text-center">
     <div class="container">
       <h1>BMC</h1>
-      <p class="lead text-muted">Her kan man finde informationer omkring hvad jeg har lært angående design</p>
+      <p class="lead text-muted">Her kan man finde informationer omkring hvad jeg har lært om Business model canvas</p>
     </div>
   </section>
 
@@ -58,41 +62,65 @@
     <!-- START THE FEATURETTES -->
 
     <hr class="featurette-divider">
-
+    <!-- BUSINESS MODEL CANVAS -->
     <div class="row featurette">
       <div class="col-md-7">
-        <h2 class="featurette-heading">First featurette heading. <span class="text-muted">It’ll blow your mind.</span></h2>
-        <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+        <h2 class="featurette-heading">Business model canvas.</h2>
+        <p class="lead">Man kan bruge BMC til at forstå en virksomhed eller en forretning. Du kan sammenkoble BMC med Gallup kompasset, minerva modellen og en swot-analyse. Den kan hjælpe dig til at komme med nye idéer eller andre perspektiver til hvordan man kan forbedre en virksomhed eller en forretning.</p>
+        <ul>
+          <li>Partnere
+            <ul>
+              <li>Er hvor man kigger efter hvilke partnere/sponsorere som den virksomhed man skal analysere har.</li>
+            </ul>
+          </li>
+          <li>Aktiviteter
+            <ul>
+              <li>Er alt det hvor virksomheden gør for at forbedre sit image samt for at komme ud til forbrugeren. Det kan f.ek.s være en app, arrangementer, lancering af et nyt produkt, markedsføring eller en form for en udstilling.</li>
+            </ul>
+          </li>
+          <li>Værdifaktorer
+            <ul>
+              <li>Er hvor man kigger hvilke gode ting som virksomheden har, som tager perspektiv fra kundens/brugerens side(synsvinkel.</li>
+            </ul>
+          </li>
+          <li>Kunderelationer
+            <ul>
+              <li>Er hvor man kigger på hvilke relationer virksomheden har med kunden/forbrugeren. Det kan være en online butik(E-handel) samt fysiske butikker selvfølgelig. Det kan også være et arrangement som har noget med det produkt som virksomheden producere, hvor kunden er interesseret i arrangementet, ikke kunne på baggrund af det produkt som virksomheden har.</li>
+            </ul>
+          </li>
+          <li>Kunderne
+            <ul>
+              <li>Kunderne er hvor man kigger efter kunder eller potentielle kunder, og det der ens målgruppe kommer ind i billedet. Der kan man bruge gallup kompasset eller minerva modellen.</li>
+            </ul>
+          </li>
+          <li>Ressourcer
+            <ul>
+              <li>Er her man kigger på hvilke ting virksomheden har adgang eller rådighed til at kunne skaffe.</li>
+            </ul>
+          </li>
+          <li>Vejen til kunderne
+            <ul>
+              <li>Er hvilke/hvilken måder/måde virksomheden kan nå ud til kunden/forbrugeren på. Det kan være sociale medier, reklame, radiospot, merchandise eller hjemmeside osv.</li>
+            </ul>
+          </li>
+          <li>Omkostninger
+            <ul>
+              <li>Er alle udgifter som virksomheden har. Butikker, produktion af produkter og merchandise. Det vil også være medarbejdernes løn, samt vedligeholdelse af butikken.</li>
+            </ul>
+          </li>
+          <li>Indtægter
+            <ul>
+              <li>Er alle former for indtægter som virksomheden kan have. Altså det vil være det som virksomheden kan tjene penge på.</li>
+            </ul>
+          </li>
+        </ul>
+        <p class="lead"><cite>-Jonas, Moodle/<a class="google" href="https://docs.google.com/document/d/1eWhzpceHeu0s55jdGhSaw5sXykOJ0x-PxXgqjlubiKc/edit?usp=sharing" target="_blank">Google docs</a>, <time datetime="2020-11-05">5 November, 2020</time></cite></p>
       </div>
       <div class="col-md-5">
-        <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 500x500"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"/><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg>
+        <img src="canvas.jpg" height="350px" width="450px" alt="">
       </div>
     </div>
-
-    <hr class="featurette-divider">
-
-    <div class="row featurette">
-      <div class="col-md-7 order-md-2">
-        <h2 class="featurette-heading">Oh yeah, it’s that good. <span class="text-muted">See for yourself.</span></h2>
-        <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
-      </div>
-      <div class="col-md-5 order-md-1">
-        <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 500x500"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"/><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg>
-      </div>
-    </div>
-
-    <hr class="featurette-divider">
-
-    <div class="row featurette">
-      <div class="col-md-7">
-        <h2 class="featurette-heading">And lastly, this one. <span class="text-muted">Checkmate.</span></h2>
-        <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
-      </div>
-      <div class="col-md-5">
-        <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 500x500"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"/><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg>
-      </div>
-    </div>
-
+    <!-- BUSINESS MODEL CANVAS SLUT -->
     <hr class="featurette-divider">
 
     <!-- /END THE FEATURETTES -->
@@ -108,10 +136,11 @@
       <i class="fab fa-instagram footer-icon"></i>
       <i class="fas fa-envelope footer-icon"></i>
 
-    <p>© Copyright 2020 Jonas Hawthorn Mikkelsen</p>
+    <p class="copyright">© Copyright 2020 Jonas Hawthorn Mikkelsen</p>
   </div>
   </footer>
 </main>
+<!-- JAVASCRIPT BRUGT TIL ALLE UNDERSIDER -->
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
       <script>window.jQuery || document.write('<script src="assets/js/vendor/jquery.slim.min.js"><\/script>')</script><script src="assets/dist/js/bootstrap.bundle.min.js"></script>
 </html>
